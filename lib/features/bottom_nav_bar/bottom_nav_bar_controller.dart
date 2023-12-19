044746fda05190e2638fe7219e096b5f00912f82
martynagrygiel <grygielmartyna439@gmail.com>
2023-12-19 05:11:33 +0100

    Merged main into MOBILE-09

--- lib/features/bottom_nav_bar/bottom_nav_bar_controller.dart
+++ lib/features/bottom_nav_bar/bottom_nav_bar_controller.dart
@@ -1,8 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'navigation_states.dart';
 
-@riverpod
+
 class BottomNavBarController extends StateNotifier<NavigationStates> {
   BottomNavBarController() : super(const NavigationStates());
 
