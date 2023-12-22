b7e7b6bb8710dc640b63b734a36aeb90e1666a35
K4PEK <kamilf827@gmail.com>
2023-12-22 12:44:19 +0100

    updated  redirection to faculty section

--- lib/features/home_view/widgets/faculty_section.dart
+++ lib/features/home_view/widgets/faculty_section.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../models/faculty_model.dart';
 import '../../../theme/app_theme.dart';
 import '../../../utils/context_extensions.dart';
+import '../../bottom_nav_bar/bottom_nav_bar_controller.dart';
 import 'faculty_box.dart';
 
-class FacultySection extends StatelessWidget {
+class FacultySection extends ConsumerWidget {
   const FacultySection({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final faculties = Faculties().getFaculties();
     return Padding(
       padding: const EdgeInsets.only(left: 24.0),
@@ -26,20 +28,25 @@ class FacultySection extends StatelessWidget {
                 ),
                 /// Section headline
                 GestureDetector(
-                  onTap: () {},
-                  child: Row(
-                    children: [
-                      Text(
-                        context.localize!.list,
-                        style: context.textTheme.boldBodyOrange,
-                      ),
-                      const SizedBox(width: 12),
-                      Icon(
-                        Icons.arrow_forward_ios_outlined,
-                        color: context.colorTheme.orangePomegranade,
-                        size: 8,
-                      )
-                    ],
+                  onTap: () {
+                    ref.read(bottomNavBarControllerProvider.notifier).onIndexChanged(2);  
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 2.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          context.localize!.list,
+                          style: context.textTheme.boldBodyOrange,
+                        ),
+                        const SizedBox(width: 12),
+                        Icon(
+                          Icons.arrow_forward_ios_outlined,
+                          color: context.colorTheme.orangePomegranade,
+                          size: 8,
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ],
