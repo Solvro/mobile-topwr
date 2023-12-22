be6c0b4d79090519eba22f385ce1fa1bdadac35e
K4PEK <kamilf827@gmail.com>
2023-12-22 19:42:20 +0100

    added gradient and cached logo image

--- lib/features/faculties_tab/widgets/faculty_card.dart
+++ lib/features/faculties_tab/widgets/faculty_card.dart
@@ -1,7 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../repositories/getDepartments.graphql.dart';
 import '../../../theme/app_theme.dart';
+import '../../../widgets/loading_widget.dart';
 
 class FacultyCard extends StatelessWidget {
   final Query$GetDepartments$departments? faculty;
@@ -13,28 +15,50 @@ class FacultyCard extends StatelessWidget {
       height: 92,
       width: double.infinity,
       decoration: BoxDecoration(
+        gradient: const LinearGradient(
+
+          colors: [Colors.blue, Colors.green],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
         color: Colors.green,
         borderRadius: BorderRadius.circular(8),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              faculty!.code,
-              style: context.textTheme.headlineWhite,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  faculty!.code,
+                  style: context.textTheme.headlineWhite,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  faculty!.name,
+                  style: context.textTheme.bodyWhite,
+                )
+              ],
             ),
-            const SizedBox(
-              height: 8,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: CachedNetworkImage(
+              imageUrl:
+                  'https://p1.hiclipart.com/preview/209/923/667/google-logo-background-g-suite-google-pay-google-doodle-text-circle-line-area-png-clipart.jpg',
+              height: double.infinity,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => const LoadingWidget(),
+              errorWidget: (context, url, error) => ErrorWidget(error),
             ),
-            Text(
-              faculty!.name,
-              style: context.textTheme.bodyWhite,
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
