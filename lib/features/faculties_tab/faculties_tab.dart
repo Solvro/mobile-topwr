c543afc4db5cbc09e57632bb606594ecfa50f916
K4PEK <kamilf827@gmail.com>
2023-12-22 18:05:34 +0100

    updated exceptions

--- lib/features/faculties_tab/faculties_tab.dart
+++ lib/features/faculties_tab/faculties_tab.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../repositories/getDepartments.graphql.dart';
 import '../../theme/app_theme.dart';
 import '../../utils/context_extensions.dart';
+import '../../widgets/loading_widget.dart';
 import 'repositories/faculties_list_provider.dart';
 import 'widgets/faculty_card.dart';
 import 'widgets/search_widget.dart';
@@ -11,33 +11,60 @@ import 'widgets/search_widget.dart';
 class FacultiesTab extends ConsumerWidget {
   const FacultiesTab({super.key});
 
+  String checkState(List faculties) {
+    if (faculties.isEmpty) {
+      return 'empty';
+    } else if (faculties[0] == null) {
+      return 'loading';
+    } else if (faculties.isNotEmpty && faculties[0] != null) {
+      return 'ready';
+    }
+    return 'error';
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final faculties = ref.watch(filteredListProvider);
     return SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 24),
-              Text(
-                context.localize!.faculties,
-                style: context.textTheme.headline,
-              ),
-              const SizedBox(height: 16),
-              const SearchWidget(),
-              const SizedBox(height: 47),
-              for (Query$GetDepartments$departments? faculty in faculties)
-                Column(
-                  children: [
-                    FacultyCard(faculty: faculty),
-                    const SizedBox(height: 16),
-                  ],
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 24),
+            Text(
+              context.localize!.faculties,
+              style: context.textTheme.headline,
+            ),
+            const SizedBox(height: 16),
+            const SearchWidget(),
+            const SizedBox(height: 47),
+            switch (checkState(faculties)) {
+              'empty' => Center(
+                  child: Text(
+                    'Pusta lista',
+                    style: context.textTheme.body,
+                  ),
                 ),
-            ],
-          ),
+              'loading' => const LoadingWidget(),
+              'error' => ErrorWidget('Error'),
+              'ready' => Column(
+                  children: faculties
+                      .map(
+                        (faculty) => Column(
+                          children: [
+                            FacultyCard(faculty: faculty),
+                            const SizedBox(height: 16),
+                          ],
+                        ),
+                      )
+                      .toList(),
+                ),
+              String() => ErrorWidget('Different error'),
+            }
+          ],
         ),
-      );
+      ),
+    );
   }
 }
--- lib/features/faculties_tab/repositories/faculties_list_provider.dart
+++ lib/features/faculties_tab/repositories/faculties_list_provider.dart
@@ -1,6 +1,7 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../../repositories/departments_repository.dart';
+import '../../../repositories/getDepartments.graphql.dart';
 import 'search_provider.dart';
 
 part 'faculties_list_provider.g.dart';
@@ -9,9 +10,20 @@ part 'faculties_list_provider.g.dart';
 List<Department?> filteredList(FilteredListRef ref) {
   final originalList = ref.watch(departmentsRepositoryProvider);
   final query = ref.watch(searchTextControllerProvider);
-  final filteredList = originalList.value!.where((element)=>
-    element!.name.toLowerCase().contains(query.toLowerCase()) ||
-    element.code.toLowerCase().contains(query.toLowerCase())
-  ).toList();
-  return filteredList;
+  if (originalList.runtimeType ==
+      AsyncLoading<List<Query$GetDepartments$departments?>?>) {
+    return [];
+  }
+  return originalList.when(
+    data: (data) {
+      final filteredList = originalList.value!
+          .where((element) =>
+              element!.name.toLowerCase().contains(query.toLowerCase()) ||
+              element.code.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+      return filteredList;
+    },
+    error: (_, __) => [],
+    loading: ()=>[null],
+  );
 }
