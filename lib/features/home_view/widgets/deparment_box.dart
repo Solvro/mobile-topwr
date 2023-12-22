4703d59d4853a5634011eacef4f810b22732bf2d
K4PEK <kamilf827@gmail.com>
2023-12-22 20:18:21 +0100

    added image

--- lib/features/home_view/widgets/deparment_box.dart
+++ lib/features/home_view/widgets/deparment_box.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../repositories/getDepartments.graphql.dart';
@@ -5,8 +6,7 @@ import '../../../theme/app_theme.dart';
 
 class DepartmentBox extends StatelessWidget {
   final Query$GetDepartments$departments? department;
-  const DepartmentBox(
-      {super.key, this.department});
+  const DepartmentBox({super.key, this.department});
 
   @override
   Widget build(BuildContext context) {
@@ -17,26 +17,43 @@ class DepartmentBox extends StatelessWidget {
         width: 120,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8),
-          color: Colors.green,
+          gradient: const LinearGradient(
+            colors: [
+              Colors.green,
+              Colors.blue,
+            ],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+          ),
         ),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Stack(
             children: [
-              Text(
-                department!.code,
-                style: context.textTheme.titleWhite,
+              CachedNetworkImage(
+                imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png',
+                color: Colors.white.withOpacity(.12),
+                fit: BoxFit.cover,
+                height: double.infinity,
               ),
-              const SizedBox(
-                height: 8,
-              ),
-              Text(
-                department!.name.length > 32
-                    ? '${department!.name.substring(0, 28)}...'
-                    : department!.name,
-                style: context.textTheme.bodyWhite,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    department!.code,
+                    style: context.textTheme.titleWhite,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    department!.name.length > 32
+                        ? '${department!.name.substring(0, 28)}...'
+                        : department!.name,
+                    style: context.textTheme.bodyWhite,
+                  ),
+                ],
               ),
             ],
           ),
