b825f0d8853d1f4040a7630fd153182ed174681f
Szymon Kowaliński <szymon@kowalinski.dev>
2024-02-22 01:00:08 +0100

    simplified scrollable loader

--- lib/widgets/loading_widgets/scrolable_loader_builder.dart
+++ lib/widgets/loading_widgets/scrolable_loader_builder.dart
@@ -18,29 +18,27 @@ class ScrollableLoaderBuilder extends StatelessWidget {
   final Axis scrollDirection;
 
   final double itemsSpacing;
+
   @override
   Widget build(BuildContext context) {
     final isHorizontal = scrollDirection == Axis.horizontal;
     final crossAxisSize = crossAxisForcedSize ?? double.infinity;
-
     return SizedBox(
         width: isHorizontal ? double.infinity : crossAxisSize,
         height: !isHorizontal ? double.infinity : crossAxisSize,
-        child: LayoutBuilder(builder: (context, constraints) {
-          final mainAxisAvailableSpace =
-              isHorizontal ? constraints.maxWidth : constraints.maxHeight;
-
-          final numberOfVisibleItems =
-              (mainAxisAvailableSpace ~/ mainAxisItemSize) + 1;
-
-          return ListView.separated(
-            scrollDirection: scrollDirection,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: numberOfVisibleItems,
-            itemBuilder: itemBuilder,
-            separatorBuilder: (context, index) =>
-                SizedBox(width: itemsSpacing, height: itemsSpacing),
-          );
-        }));
+        child: ListView.builder(
+          scrollDirection: scrollDirection,
+          physics: const NeverScrollableScrollPhysics(),
+          itemBuilder: (context, index) {
+            /// it's a workaround for ListView.separated, but without itemCount (infinite scrolling)
+            /// for some reasons ListView.builder can be infinite, and ListView.separated not
+            if (index.isEven) return itemBuilder(context, index ~/ 2);
+            return _Gap(itemsSpacing);
+          },
+        ));
   }
+}
+
+class _Gap extends SizedBox {
+  const _Gap(double space) : super(height: space, width: space);
 }
