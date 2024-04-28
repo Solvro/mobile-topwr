3cad0289f24b7c4e376d1c3c3c53087d859da33b
Szymon Kowaliński <szymon@kowalinski.dev>
2024-04-28 15:06:32 +0200

    fix: ttl durations (thirtyDays and deleted 3months option)

--- lib/api_base/ttl/ttl_config.dart
+++ lib/api_base/ttl/ttl_config.dart
@@ -18,19 +18,18 @@ enum TtlKey {
 abstract class TtlStrategy {
   static const day = Duration(days: 1);
   static const week = Duration(days: 7);
-  static const month = Duration(days: 28);
-  static const threeMonths = Duration(days: 90);
+  static const thirtyDays = Duration(days: 30);
 
   /// generated map ensures every TtlKey has its own TTL-Duration
   static const _values = UnmodifiableTtlKeyMap(
     /// specific values are yet ment to be accordingly adjusted
     infosPreviewRepository: day,
     examSessionCountdownRepository: day,
-    sciCirclesPreviewRepository: month,
-    sciCirclesRepository: month,
-    tagsRepository: month,
-    mapBuildingsRepository: month,
-    departmentsRepository: threeMonths,
+    sciCirclesPreviewRepository: thirtyDays,
+    sciCirclesRepository: thirtyDays,
+    tagsRepository: thirtyDays,
+    mapBuildingsRepository: thirtyDays,
+    departmentsRepository: thirtyDays,
   );
 
   static Duration get(TtlKey key) {
