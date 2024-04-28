3c07b9af5dd164c851b9cbfdcd9f09dce42f7c4a
Szymon Kowaliński <szymon@kowalinski.dev>
2024-04-28 16:03:55 +0200

    refactor: whereNonNull to sync* notation

--- lib/utils/where_non_null_iterable.dart
+++ lib/utils/where_non_null_iterable.dart
@@ -1,46 +1,18 @@
-class WhereNonNullIterable<T> extends Iterable<T> {
-  final Iterable<T?> _source;
-
-  /// A custom iterable class that filters out null elements from the source iterable.
-  WhereNonNullIterable(this._source);
-
-  @override
-  Iterator<T> get iterator => _WhereNonNullIterator<T>(_source);
-}
-
-class _WhereNonNullIterator<T> implements Iterator<T> {
-  final Iterator<T?> _sourceIterator;
-
-  /// A private iterator class used by WhereNonNullIterable to iterate over non-null elements.
-  _WhereNonNullIterator(Iterable<T?> source)
-      : _sourceIterator = source.iterator;
-
-  @override
-  T get current {
-    return _sourceIterator.current as T;
-  }
-
-  @override
-  // Moves to the next non-null element in the iteration.
-  bool moveNext() {
-    while (_sourceIterator.moveNext()) {
-      if (_sourceIterator.current != null) {
-        return true;
-      }
+Iterable<T> _whereNonNullIterable<T>(Iterable<T?> source) sync* {
+  for (final element in source) {
+    if (element != null) {
+      yield element;
     }
-    return false;
   }
 }
 
 extension WhereNonNullExt<T> on Iterable<T?> {
-  /// filters out null elements using custom WhereNonNullIterable
   Iterable<T> get whereNonNull {
-    return WhereNonNullIterable(this);
+    return _whereNonNullIterable(this);
   }
 }
 
 extension WhereNonNullExtNullable<T> on Iterable<T?>? {
-  /// filters out null elements using custom WhereNonNullIterable
   Iterable<T> get whereNonNull {
     if (this == null) return [];
     return this!.whereNonNull;
