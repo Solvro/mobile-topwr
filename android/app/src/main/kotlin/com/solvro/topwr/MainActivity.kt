1218d40d12e42cbabffdb02bf6bdfd263b8f26b0
amtrax <mikics2k17@gmail.com>
2024-05-15 23:04:24 +0200

    - fixed Android: back button/gesture kills app  ONLY WORKS FOR ANDROID VERSIONS < 14

--- android/app/src/main/kotlin/com/solvro/topwr/MainActivity.kt
+++ android/app/src/main/kotlin/com/solvro/topwr/MainActivity.kt
@@ -1,6 +1,24 @@
 package com.solvro.topwr
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "topwr.app.android.channel"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            // This method is invoked on the main thread.
+                call, result ->
+            if (call.method == "putAppInBackground") {
+                moveTaskToBack(true)
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
 }
