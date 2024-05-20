b63ca62d9154a0a68dacbe80f37eb2d957392600
Szymon Kowaliński <szymon@kowalinski.dev>
2024-05-20 11:23:18 +0200

    fix: overlapping max labels on chart

--- lib/features/iparking_chart/chart_elements/labels_left.dart
+++ lib/features/iparking_chart/chart_elements/labels_left.dart
@@ -3,20 +3,30 @@ import 'package:flutter/widgets.dart';
 
 import '../../../theme/app_theme.dart';
 
+extension WillMaxLabelOverlap on TitleMeta {
+  bool get isMaxLabelOverlapping {
+    return max % appliedInterval ~/ 10 == 0;
+  }
+}
+
 class LeftLabels extends AxisTitles {
   LeftLabels(BuildContext context)
       : super(
           sideTitles: SideTitles(
-            showTitles: true,
-            reservedSize: 30,
-            getTitlesWidget: (value, meta) => SideTitleWidget(
-              axisSide: AxisSide.left,
-              space: 10,
-              child: Text(
-                meta.formattedValue,
-                style: context.iParkingTheme.chart,
-              ),
-            ),
-          ),
+              showTitles: true,
+              reservedSize: 30,
+              getTitlesWidget: (value, meta) {
+                if (value == meta.max && meta.isMaxLabelOverlapping) {
+                  return const SizedBox.shrink();
+                }
+                return SideTitleWidget(
+                  axisSide: AxisSide.left,
+                  space: 10,
+                  child: Text(
+                    meta.formattedValue,
+                    style: context.iParkingTheme.chart,
+                  ),
+                );
+              }),
         );
 }
