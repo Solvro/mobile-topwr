33faf74d7774ae91c6856fc89f33d5c4385e8ba6
Szymon Kowaliński <szymon@kowalinski.dev>
2024-05-22 23:47:16 +0200

    fix: solved deprecation of WillPopScope

--- lib/features/bottom_nav_bar/bottom_nav_bar.dart
+++ lib/features/bottom_nav_bar/bottom_nav_bar.dart
@@ -1,11 +1,11 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'nested_navigator.dart';
+
 import '../../theme/app_theme.dart';
+import 'android_pop_bug_workaround.dart';
 import 'bottom_nav_bar_controller.dart';
 import 'nav_bar_config.dart';
+import 'nested_navigator.dart';
 
 class RootViewWithBottomNavBar extends ConsumerWidget {
   const RootViewWithBottomNavBar({super.key});
@@ -30,53 +30,44 @@ class RootViewWithBottomNavBar extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    const platform = MethodChannel('topwr.app.android.channel');
     final selectedTab = ref.watch(bottomNavBarControllerProvider);
     final navigator = ref.watch(navigatorProvider);
-    return
-         WillPopScope(
-      onWillPop: () async {
-          if(Platform.isAndroid && (navigator.navigatorKey.currentState!.canPop() == false)) {
-            platform.invokeMethod<int>('putAppInBackground');
-            return false;
-          }
-        else {
-          return true;
-        }
-      },
-        child: Scaffold(
-            body: NavigatorPopHandler(
-              onPop: navigator.handleNestedPop,
-              child: Navigator(
-                key: navigator.navigatorKey,
-                onGenerateRoute: navigator.onGenerateRoute,
-              ),
+    return Scaffold(
+        body: PopScope(
+          canPop: !navigator.androidSpecialPopTreatment,
+          child: NavigatorPopHandler(
+            onPop: navigator.handleNestedPop,
+            child: Navigator(
+              key: navigator.navigatorKey,
+              onGenerateRoute: navigator.onGenerateRoute,
             ),
-            bottomNavigationBar: Theme(
-                data: Theme.of(context).copyWith(
-                  splashColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                ),
-                child: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: <BoxShadow>[
-                        BoxShadow(
-                          color: Colors.black.withOpacity(.08),
-                          blurRadius: 20,
-                          offset: const Offset(0, -1),
-                        ),
-                      ],
+          ),
+        ),
+        bottomNavigationBar: Theme(
+            data: Theme.of(context).copyWith(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+            ),
+            child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.black.withOpacity(.08),
+                      blurRadius: 20,
+                      offset: const Offset(0, -1),
                     ),
-                    child: BottomNavigationBar(
-                      currentIndex: selectedTab.index,
-                      onTap: ref
-                          .watch(bottomNavBarControllerProvider.notifier)
-                          .onIndexChanged,
-                      backgroundColor: context.colorTheme.greyLight,
-                      showSelectedLabels: false,
-                      showUnselectedLabels: false,
-                      type: BottomNavigationBarType.fixed,
-                      items: getNavigationBarItems(context, selectedTab),
-                    )))));
+                  ],
+                ),
+                child: BottomNavigationBar(
+                  currentIndex: selectedTab.index,
+                  onTap: ref
+                      .watch(bottomNavBarControllerProvider.notifier)
+                      .onIndexChanged,
+                  backgroundColor: context.colorTheme.greyLight,
+                  showSelectedLabels: false,
+                  showUnselectedLabels: false,
+                  type: BottomNavigationBarType.fixed,
+                  items: getNavigationBarItems(context, selectedTab),
+                ))));
   }
 }
