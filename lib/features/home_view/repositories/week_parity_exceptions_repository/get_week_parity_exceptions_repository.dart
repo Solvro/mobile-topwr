8495b89e6b26ee5cc257f98b3f41c01ace207ac9
Szymon Kowaliński <szymon@kowalinski.dev>
2024-05-27 17:08:05 +0200

    feat: updated queries

--- lib/features/home_view/repositories/week_parity_exceptions_repository/get_week_parity_exceptions_repository.dart
+++ lib/features/home_view/repositories/week_parity_exceptions_repository/get_week_parity_exceptions_repository.dart
@@ -6,14 +6,16 @@ import 'getWeekParityExceptions.graphql.dart';
 
 part 'get_week_parity_exceptions_repository.g.dart';
 
-typedef WeekParityExceptions = Query$GetWeekParityExceptions$weekDayException$weekday;
+typedef WeekParityExceptions
+    = Query$GetWeekParityExceptions$weekDayException$weekday;
 
 @riverpod
 Stream<List<WeekParityExceptions?>?> getWeekParityExceptionsRepository(
     GetWeekParityExceptionsRepositoryRef ref) async* {
-  final stream = ref.watchQueryWithCache(
-    WatchOptions$Query$GetWeekParityExceptions(eagerlyFetchResults: true),
-    TtlKey.weekParityExceptionsRepository,
-  );
-  yield* stream.map((event) => event?.weekDayException?.weekday);
+  // final stream = ref.watchQueryWithCache(
+  //   WatchOptions$Query$GetWeekParityExceptions(eagerlyFetchResults: true),
+  //   TtlKey.weekParityExceptionsRepository,
+  // );
+  // yield* stream.map((event) => event?.weekDayException?.weekday);
+  yield* Stream.value(null); // WORKAROUND
 }
