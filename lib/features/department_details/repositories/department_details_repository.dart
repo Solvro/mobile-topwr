32f5cabe8076b0d860bc36dec8a06f3885b610a9
mikolaj-jalocha <mikics2k17@gmail.com>
2024-07-19 12:11:26 +0200

    api configuration with repository

---
+++ lib/features/department_details/repositories/department_details_repository.dart
@@ -0,0 +1,19 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import '../../../api_base/watch_query_adapter.dart';
+import '../../../config/ttl_config.dart';
+import 'getDepartmentDetails.graphql.dart';
+
+part 'department_details_repository.g.dart';
+
+typedef DepartmentDetails = Query$GetDepartmentDetails;
+typedef _Vars = Variables$Query$GetDepartmentDetails;
+@riverpod
+Stream<DepartmentDetails?> departmentDetailsRepository(
+    DepartmentDetailsRepositoryRef ref, String id) async* {
+  final stream = ref.watchQueryWithCache(
+    WatchOptions$Query$GetDepartmentDetails(eagerlyFetchResults: true, variables: _Vars(id: id)),
+    TtlKey.departmentsDetailsRepository,
+  );
+  yield* stream.map((event) => event);
+  }
+
