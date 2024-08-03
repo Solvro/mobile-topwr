0245c6d5935a7fe4fc2407795f5d8bbe61e78dd8
Szymon Kowaliński <szymon@kowalinski.dev>
2024-08-03 19:30:33 +0200

    fix(navigation): reverse detail transition builders to default ones

--- lib/config/transitions.dart
+++ lib/config/transitions.dart
@@ -3,9 +3,6 @@ import "package:auto_route/auto_route.dart";
 // TODO(simon-the-shark): adjust this settings if desired
 abstract class TransitionsConfig {
   static const durationInMiliseconds = 200;
-  static const detailDurationInMiliseconds = 200;
   static const slideLeftBuilder = TransitionsBuilders.slideLeftWithFade;
   static const slideRightBuilder = TransitionsBuilders.slideRightWithFade;
-  static const fallbackBuilder = TransitionsBuilders.fadeIn;
-  static const detailViewBuilder = TransitionsBuilders.slideBottom;
 }
