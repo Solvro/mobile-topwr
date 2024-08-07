5ce1a09dfaa7e001e629869eaddc4d00f5608c44
mikolaj-jalocha <mikics2k17@gmail.com>
2024-08-07 21:25:19 +0200

    #128 - implement guide detail view - setup front end layer

---
+++ lib/features/guide_detail_view/guide_detail_view.dart
@@ -0,0 +1,110 @@
+import "package:auto_route/auto_route.dart";
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart";
+
+import "../../api_base/directus_assets_url.dart";
+import "../../theme/app_theme.dart";
+import "../../utils/context_extensions.dart";
+import "../../widgets/detail_views/detail_view_app_bar.dart";
+import "../../widgets/my_cached_image.dart";
+import "../../widgets/my_error_widget.dart";
+import "repository/guide_detail_view_repository.dart";
+
+@RoutePage()
+class GuideDetailView extends StatelessWidget {
+  const GuideDetailView({
+    @PathParam("id") required this.id,
+    super.key,
+  });
+
+  final String id;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: DetailViewAppBar(context, title: context.localize.guide),
+      body: _GuideDetailDataView(id: id),
+    );
+  }
+}
+
+class _GuideDetailDataView extends ConsumerWidget {
+  final String id;
+
+  const _GuideDetailDataView({super.key, required this.id});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(guideDetailsRepositoryProvider(id));
+    return switch (state) {
+      AsyncLoading() => const Placeholder(),
+      AsyncError(:final error) => MyErrorWidget(error),
+      AsyncValue(:final value) => ListView(
+          children: [
+            MyCachedImage(value?.cover?.filename_disk?.directusUrl),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: HtmlWidget(
+                value?.description ?? "",
+                textStyle: context.textTheme.body.copyWith(fontSize: 16),
+              ),
+            ),
+            ListView.separated(
+              shrinkWrap: true,
+              physics: const ClampingScrollPhysics(),
+              padding: const EdgeInsets.only(bottom: 32),
+              itemCount: value?.questions?.length ?? 0,
+              itemBuilder: (context, index) {
+                final question = value?.questions?[index]?.FAQ_id;
+                return _MyExpansionTile(
+                  title: question?.question ?? "",
+                  description: question?.answer ?? "",
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 8,
+              ),
+            )
+          ],
+        )
+    };
+  }
+}
+
+class _MyExpansionTile extends StatelessWidget {
+  final String title;
+  final String description;
+
+  const _MyExpansionTile(
+      {super.key, required this.title, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: ExpansionTile(
+        title: Text(title, style: context.textTheme.title),
+        backgroundColor: context.colorTheme.greyLight,
+        collapsedBackgroundColor: context.colorTheme.greyLight,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        collapsedShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        iconColor: context.colorTheme.orangePomegranade,
+        collapsedIconColor: context.colorTheme.orangePomegranade,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4),
+            child: ListTile(
+              title: Text(description,
+                  style: context.textTheme.body.copyWith(fontSize: 14)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
