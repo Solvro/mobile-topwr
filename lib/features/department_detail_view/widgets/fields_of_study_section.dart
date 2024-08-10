27c7a9155cb62d7f08feb05411d42018f2d8808c
Szymon Kowaliński <szymon@kowalinski.dev>
2024-08-11 01:29:27 +0200

    feat(department-details): add exapanded tiles for field of studies

--- lib/features/department_detail_view/widgets/fields_of_study_section.dart
+++ lib/features/department_detail_view/widgets/fields_of_study_section.dart
@@ -1,17 +1,26 @@
 import "package:flutter/cupertino.dart";
-import "package:separate/separate.dart";
 
 import "../../../theme/app_theme.dart";
 import "../../../utils/context_extensions.dart";
-import "../../../widgets/wide_tile_card.dart";
+import "../repository/getDepartmentDetails.graphql.dart";
+import "field_of_study_expansion_tile.dart";
+
+typedef FieldOfStudy
+    = Query$GetDepartmentDetails$Departments_by_id$fieldsOfStudies;
 
 class FieldsOfStudySection extends StatelessWidget {
   const FieldsOfStudySection({super.key, required this.fieldsOfStudy});
 
-  final List<String> fieldsOfStudy;
+  final List<FieldOfStudy> fieldsOfStudy;
 
   @override
   Widget build(BuildContext context) {
+    final firstDegree = fieldsOfStudy
+        .where((item) => !item.is2ndDegree && !item.isLongCycleStudies);
+    final secondDegree = fieldsOfStudy
+        .where((item) => item.is2ndDegree && !item.isLongCycleStudies);
+    final longCycle = fieldsOfStudy.where((item) => item.isLongCycleStudies);
+
     return Padding(
       padding: const EdgeInsets.only(left: 24, right: 24, top: 24, bottom: 8),
       child: Column(
@@ -21,14 +30,23 @@ class FieldsOfStudySection extends StatelessWidget {
             context.localize.fields_of_study,
             style: context.textTheme.headline,
           ),
-          const SizedBox(height: 16),
-          ...fieldsOfStudy
-              .map<Widget>(
-                (item) => WideTileCard(title: item),
-              )
-              .separate(
-                (i, e0, e1) => const SizedBox(height: 12),
-              ),
+          if (firstDegree.isNotEmpty)
+            FieldOfStudyExpansionTile(
+              title: context.localize.first_degree,
+              fieldsOfStudy: firstDegree,
+              initiallyExpanded: true,
+            ),
+          if (secondDegree.isNotEmpty)
+            FieldOfStudyExpansionTile(
+              title: context.localize.second_degree,
+              fieldsOfStudy: secondDegree,
+            ),
+          if (longCycle.isNotEmpty)
+            FieldOfStudyExpansionTile(
+              title: context.localize.long_cycle,
+              fieldsOfStudy: longCycle,
+              initiallyExpanded: true,
+            ),
         ],
       ),
     );
