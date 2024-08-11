23095b392ce2288198afc69c45b1267e2b9083e7
Szymon Kowaliński <szymon@kowalinski.dev>
2024-08-11 02:00:59 +0200

    feat(deparment-details): add FoS url launching

--- lib/features/department_detail_view/widgets/field_of_study_expansion_tile.dart
+++ lib/features/department_detail_view/widgets/field_of_study_expansion_tile.dart
@@ -3,7 +3,7 @@ import "package:separate/separate.dart";
 
 import "../../../theme/app_theme.dart";
 import "../../../widgets/my_expansion_tile.dart";
-import "../../../widgets/wide_tile_card.dart";
+import "field_of_study_tile.dart";
 import "fields_of_study_section.dart";
 
 class FieldOfStudyExpansionTile extends StatelessWidget {
@@ -18,8 +18,6 @@ class FieldOfStudyExpansionTile extends StatelessWidget {
   final bool initiallyExpanded;
   @override
   Widget build(BuildContext context) {
-    const fontSize = 18.0;
-    final emojiStyle = context.textTheme.headline.copyWith(fontSize: 18);
     return Padding(
       padding: const EdgeInsets.only(top: 16),
       child: MyExpansionTile(
@@ -27,32 +25,7 @@ class FieldOfStudyExpansionTile extends StatelessWidget {
         initiallyExpanded: initiallyExpanded,
         title: title,
         children: fieldsOfStudy
-            .map<Widget>(
-              (item) => WideTileCard(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                title: item.name,
-                trailing: Row(
-                  children: <Widget>[
-                    if (item.isEnglish)
-                      Text("🇬🇧", style: emojiStyle)
-                    else
-                      Text("🇵🇱", style: emojiStyle),
-                    Icon(
-                      Icons.sunny,
-                      color: context.colorTheme.greyPigeon,
-                      size: fontSize,
-                    ),
-                    if (item.hasWeekendModeOption)
-                      Icon(
-                        Icons.remove_red_eye_outlined,
-                        color: context.colorTheme.greyPigeon,
-                        size: fontSize,
-                      ),
-                    const SizedBox.shrink(),
-                  ].separate((i, e0, e1) => const SizedBox(width: 4)),
-                ),
-              ),
-            )
+            .map<Widget>(FieldOfStudyTile.new)
             .separate((i, e0, e1) => const SizedBox(height: 12))
             .toList(),
       ),
