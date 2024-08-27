ef3847ca5139eb30f6a8bc69418c6f2ca707deea
mikolaj-jalocha <mikics2k17@gmail.com>
2024-08-27 12:31:29 +0200

    #159 - simple error, not found, offline animations - implement error

--- lib/widgets/my_error_widget.dart
+++ lib/widgets/my_error_widget.dart
@@ -1,14 +1,20 @@
 import "package:flutter/material.dart";
 import "package:logger/logger.dart";
+import "package:lottie/lottie.dart";
 
 import "../api_base/watch_query_adapter.dart";
+import "../config/ui_config.dart";
 import "../features/offline_messages/widgets/grapgql_offline_message.dart";
 import "../features/parkings_view/api_client/iparking_commands.dart";
 import "../features/parkings_view/widgets/offline_parkings_view.dart";
+import "../gen/assets.gen.dart";
+import "../theme/app_theme.dart";
 
 class MyErrorWidget extends StatelessWidget {
   const MyErrorWidget(this.error, {super.key});
+
   final Object? error;
+
   @override
   Widget build(BuildContext context) {
     Logger().e(error.toString());
@@ -16,10 +22,23 @@ class MyErrorWidget extends StatelessWidget {
       ParkingsOfflineException() => const OfflineParkingsView(),
       GqlOfflineException(:final ttlKey) => OfflineGraphQLMessage(ttlKey),
       _ => Center(
-          child: Text(
-            error.toString(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Lottie.asset(
+                renderCache: RenderCache.raster,
+                repeat: false,
+                Assets.animations.error,
+                frameRate: const FrameRate(LottieAnimationConfig.frameRate),
+              ),
+              Text(
+                textAlign: TextAlign.center,
+                style: context.textTheme.headline,
+                error.toString(),
+              ),
+            ],
           ),
-        )
+        ),
     };
   }
 }
