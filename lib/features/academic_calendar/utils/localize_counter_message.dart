f459a6ccdc210225e85de4e02e61499fa6b8cc15
mikolaj-jalocha <mikics2k17@gmail.com>
2024-09-04 12:59:42 +0200

    #183 - add days to semester mode - cleanup

--- lib/features/academic_calendar/utils/localize_counter_message.dart
+++ lib/features/academic_calendar/utils/localize_counter_message.dart
@@ -6,8 +6,10 @@ import "../repository/academic_calendar_repo.dart";
 
 extension LocalizeCounterMessageExtension on AcademicCalendar {
   String localizeDaysCounterMessage(BuildContext context) {
-    if (this.AcademicCalendarData!.isHolidays()) return context.localize.until_the_end_of_the_semester_break;
-    if (this.AcademicCalendarData!.isExamSession()) return context.localize.until_the_session_ends;
+    if (this.AcademicCalendarData!.isHolidays())
+      return context.localize.until_the_end_of_the_semester_break;
+    if (this.AcademicCalendarData!.isExamSession())
+      return context.localize.until_the_session_ends;
     return context.localize.days;
   }
 }
