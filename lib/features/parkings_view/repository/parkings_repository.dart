a2f45465280beba8a00422a48cbb9e7c13dda702
Tomasz Trela <153635094+TTomaszPWR@users.noreply.github.com>
2024-09-05 15:11:58 +0200

    feat/ place fav parking on top of parkings list

--- lib/features/parkings_view/repository/parkings_repository.dart
+++ lib/features/parkings_view/repository/parkings_repository.dart
@@ -7,6 +7,7 @@ import "package:riverpod_annotation/riverpod_annotation.dart";
 import "../api_client/iparking_client.dart";
 import "../api_client/iparking_commands.dart";
 import "../models/parking.dart";
+import "local_fav_parking_repository.dart";
 
 part "parkings_repository.g.dart";
 
@@ -26,6 +27,21 @@ Future<IList<Parking>> parkingsRepository(ParkingsRepositoryRef ref) async {
   final response = await ref.postIParkingCommand<Map<String, dynamic>>(
     FetchPlacesCommand(DateTime.now()),
   );
-  final list = response.data?["places"] as List<dynamic>;
-  return list.whereType<Map<String, dynamic>>().map(Parking.fromJson).toIList();
+  final parkings = response.data?["places"] as List<dynamic>;
+  final List<Parking> list = parkings.whereType<Map<String, dynamic>>().map(Parking.fromJson).toList();
+  
+  return _sortParkingsByFav(list, ref).toIList();
+}
+
+List<Parking> _sortParkingsByFav(List<Parking> list, ParkingsRepositoryRef ref){
+  final List<Parking> finalParkings = [];
+  for (final parking in list) {
+    final isFavorite = ref.watch(localFavParkingsRepositoryProvider(parking.id)) ?? false;
+    if(isFavorite) {
+      finalParkings.insert(0, parking);
+    }else{
+      finalParkings.add(parking);
+    }
+  }
+  return finalParkings;
 }
