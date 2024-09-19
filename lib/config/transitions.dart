7f82437c583d975415ff98a1c074359748f45c57
Szymon Kowaliński <szymon@kowalinski.dev>
2024-09-19 11:57:07 +0200

    feat(bottom-nav-bar): remove view transitions

--- lib/config/transitions.dart
+++
@@ -1,8 +0,0 @@
-import "package:auto_route/auto_route.dart";
-
-// TODO(simon-the-shark): adjust this settings if desired
-abstract class TransitionsConfig {
-  static const durationInMiliseconds = 200;
-  static const slideLeftBuilder = TransitionsBuilders.slideLeft;
-  static const slideRightBuilder = TransitionsBuilders.slideRight;
-}
--- lib/features/navigator/app_router.dart
+++ lib/features/navigator/app_router.dart
@@ -3,7 +3,6 @@ import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
-import "../../config/transitions.dart";
 import "../about_us_view/about_us_view.dart";
 import "../buildings_view/buildings_view.dart";
 import "../department_detail_view/department_detail_view.dart";
@@ -15,7 +14,6 @@ import "../parkings_view/parkings_view.dart";
 import "../science_club_detail_view/science_club_detail_view.dart";
 import "../science_clubs_view/science_clubs_view.dart";
 import "root_view.dart";
-import "utils/tab_bar_transition.dart";
 
 part "app_router.g.dart";
 part "app_router.gr.dart";
@@ -35,37 +33,22 @@ class AppRouter extends _$AppRouter {
             CustomRoute(
               path: "",
               page: HomeRoute.page,
-              durationInMilliseconds: TransitionsConfig.durationInMiliseconds,
-              reverseDurationInMilliseconds:
-                  TransitionsConfig.durationInMiliseconds,
-              transitionsBuilder: ref.tabBarTransitionBuilder(HomeRoute.name),
+              durationInMilliseconds: 0,
             ),
             CustomRoute(
-              path: "map",
+              path: "buildings",
               page: BuildingsRoute.page,
-              durationInMilliseconds: TransitionsConfig.durationInMiliseconds,
-              reverseDurationInMilliseconds:
-                  TransitionsConfig.durationInMiliseconds,
-              transitionsBuilder:
-                  ref.tabBarTransitionBuilder(BuildingsRoute.name),
+              durationInMilliseconds: 0,
             ),
             CustomRoute(
               path: "parkings",
               page: ParkingsRoute.page,
-              durationInMilliseconds: TransitionsConfig.durationInMiliseconds,
-              reverseDurationInMilliseconds:
-                  TransitionsConfig.durationInMiliseconds,
-              transitionsBuilder:
-                  ref.tabBarTransitionBuilder(ParkingsRoute.name),
+              durationInMilliseconds: 0,
             ),
             CustomRoute(
               path: "departments",
               page: DepartmentsRoute.page,
-              durationInMilliseconds: TransitionsConfig.durationInMiliseconds,
-              reverseDurationInMilliseconds:
-                  TransitionsConfig.durationInMiliseconds,
-              transitionsBuilder:
-                  ref.tabBarTransitionBuilder(DepartmentsRoute.name),
+              durationInMilliseconds: 0,
             ),
             AutoRoute(
               path: "departments/:id",
@@ -74,11 +57,7 @@ class AppRouter extends _$AppRouter {
             CustomRoute(
               path: "sci-clubs",
               page: ScienceClubsRoute.page,
-              durationInMilliseconds: TransitionsConfig.durationInMiliseconds,
-              reverseDurationInMilliseconds:
-                  TransitionsConfig.durationInMiliseconds,
-              transitionsBuilder:
-                  ref.tabBarTransitionBuilder(ScienceClubsRoute.name),
+              durationInMilliseconds: 0,
             ),
             AutoRoute(
               path: "sci-clubs/:id",
@@ -87,10 +66,7 @@ class AppRouter extends _$AppRouter {
             CustomRoute(
               path: "guide",
               page: GuideRoute.page,
-              durationInMilliseconds: TransitionsConfig.durationInMiliseconds,
-              reverseDurationInMilliseconds:
-                  TransitionsConfig.durationInMiliseconds,
-              transitionsBuilder: ref.tabBarTransitionBuilder(GuideRoute.name),
+              durationInMilliseconds: 0,
             ),
             AutoRoute(
               path: "aboutUs",
