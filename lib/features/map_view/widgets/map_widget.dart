5edf5284fb8cf8ad87823ddab210eae1c0812272
Szymon Kowaliński <szymon@kowalinski.dev>
2024-09-20 18:27:10 +0200

    feat(map-view): implement CancellableNetworkTileProvider

--- lib/features/map_view/widgets/map_widget.dart
+++ lib/features/map_view/widgets/map_widget.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:flutter_map/flutter_map.dart";
+import "package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
 import "../../../config/map_view_config.dart";
@@ -23,12 +24,11 @@ class MapWidget<T extends GoogleNavigable> extends ConsumerWidget {
             ref.watch(context.mapController<T>().notifier).onMapBackgroundTap,
       ),
       mapController: ref.watch(context.mapController<T>()),
-      children: [
-        TileLayer(
-          urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-          userAgentPackageName: Platform.isIOS
-              ? "com.solvro.ToPwr"
-              : "com.solvro.topwr", // not sure why this is needed; but is recommended in the docs
+          urlTemplate: OpenStreetMapConfig.tileUrl,
+          userAgentPackageName: OpenStreetMapConfig.userAgent,
+          userAgentPackageName:
+              Platform.isIOS ? "com.solvro.ToPwr" : "com.solvro.topwr",
+          tileProvider: CancellableNetworkTileProvider(),
         ),
         MarkersConsumerLayer<T>(),
         const OpenMapAtrribution(),
