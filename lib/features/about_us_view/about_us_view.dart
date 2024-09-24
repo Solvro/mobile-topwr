938d0e0816f40c740ab7d25dfcaa681c005060cb
Szymon Kowaliński <szymon@kowalinski.dev>
2024-09-24 11:20:38 +0200

    refactor(about-us): remove shimmer wrapper

--- lib/features/about_us_view/about_us_view.dart
+++ lib/features/about_us_view/about_us_view.dart
@@ -7,7 +7,6 @@ import "../../utils/context_extensions.dart";
 import "../../widgets/detail_views/detail_view_app_bar.dart";
 import "../../widgets/detail_views/sliver_header_section.dart";
 import "../../widgets/loading_widgets/scrolable_loader_builder.dart";
-import "../../widgets/loading_widgets/shimmer_loading.dart";
 import "../../widgets/loading_widgets/simple_previews/preview_card_loading.dart";
 import "../../widgets/my_error_widget.dart";
 import "models/about_us_details.dart";
@@ -76,43 +75,38 @@ class _AboustUsLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Shimmer(
-      linearGradient: shimmerGradient,
-      child: ListView(
-        physics: const NeverScrollableScrollPhysics(),
-        children: [
-          const PreviewCardLoading(
+    return ListView(
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        const PreviewCardLoading(
+          width: double.infinity,
+          height: 300,
+        ),
+        const Padding(
+          padding: EdgeInsets.all(AboutUsConfig.defaultPadding),
+          child: PreviewCardLoading(
             width: double.infinity,
-            height: 300,
+            height: 150,
           ),
-          const Padding(
-            padding: EdgeInsets.all(AboutUsConfig.defaultPadding),
-            child: PreviewCardLoading(
-              width: double.infinity,
-              height: 150,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AboutUsConfig.defaultPadding),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height / 4,
+            child: ScrollableLoaderBuilder(
+              itemsSpacing: 8,
+              scrollDirection: Axis.vertical,
+              mainAxisItemSize: 16,
+              itemBuilder: (BuildContext context, int index) {
+                return const PreviewCardLoading(
+                  width: double.infinity,
+                  height: AboutUsConfig.photoSize,
+                );
+              },
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(AboutUsConfig.defaultPadding),
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height / 4,
-              child: ShimmerLoadingItem(
-                child: ScrollableLoaderBuilder(
-                  itemsSpacing: 8,
-                  scrollDirection: Axis.vertical,
-                  mainAxisItemSize: 16,
-                  itemBuilder: (BuildContext context, int index) {
-                    return const PreviewCardLoading(
-                      width: double.infinity,
-                      height: AboutUsConfig.photoSize,
-                    );
-                  },
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
