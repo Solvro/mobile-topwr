e1ed1c158a189d08d86bb6e47ae804ea0f787331
Kamil Fedio <kamilf827@gmail.com>
2024-10-14 22:54:43 +0200

    fix: change method to filter list

--- lib/features/guide_detail_view/utils/get_the_latest_date.dart
+++ lib/features/guide_detail_view/utils/get_the_latest_date.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:intl/intl.dart";
 
+import "../../../utils/where_non_null_iterable.dart";
 import "../repository/getGuideDetails.graphql.dart";
 
 extension GetTheLatestDateGuide on BuildContext {
@@ -8,8 +9,8 @@ extension GetTheLatestDateGuide on BuildContext {
     List<Query$GetGuideDetails$FAQ_Types_by_id$questions?>? questions,
   }) {
     final DateTime? newestDate = questions
-        ?.where((e) => e?.FAQ_id?.date_created != null)
-        .map((e) => e!.FAQ_id!.date_created!)
+        ?.whereNonNull
+        .map((e) => e.FAQ_id!.date_created!)
         .reduce((a, b) => a.isAfter(b) ? a : b);
 
     final DateFormat formatter = DateFormat("dd.MM.yyyy");
@@ -20,8 +21,8 @@ extension GetTheLatestDateGuide on BuildContext {
     List<Query$GetGuideDetails$FAQ_Types_by_id$questions?>? questions,
   }) {
     final DateTime? newestDate = questions
-        ?.where((e) => e?.FAQ_id?.date_updated != null)
-        .map((e) => e!.FAQ_id!.date_updated!)
+        ?.whereNonNull
+        .map((e) => e.FAQ_id!.date_updated!)
         .reduce((a, b) => a.isAfter(b) ? a : b);
 
     final DateFormat formatter = DateFormat("dd.MM.yyyy");
