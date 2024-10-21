21a8d044c1d07c22126a98c7be61976a73d92323
Szymon Kowaliński <szymon@kowalinski.dev>
2024-10-21 21:06:14 +0200

    fix(guide-details): null safe crash

--- lib/features/guide_detail_view/utils/get_the_latest_date.dart
+++ lib/features/guide_detail_view/utils/get_the_latest_date.dart
@@ -4,25 +4,25 @@ import "package:intl/intl.dart";
 import "../../../utils/where_non_null_iterable.dart";
 import "../repository/getGuideDetails.graphql.dart";
 
-extension GetTheLatestDateGuide on BuildContext {
-  String? getTheLatesCreatedDateGuideX({
+extension GetTheLatestDateGuideX on BuildContext {
+  String? getTheLatesCreatedDateGuide({
     List<Query$GetGuideDetails$FAQ_Types_by_id$questions?>? questions,
   }) {
-    final DateTime? newestDate = questions?.whereNonNull
-        .map((e) => e.FAQ_id!.date_created!)
-        .reduce((a, b) => a.isAfter(b) ? a : b);
-
+    final newestDate = questions?.whereNonNull
+        .map(
+          (e) => e.FAQ_id?.date_created,
+        )
+        .reduce((a, b) => (b == null || a != null && a.isAfter(b)) ? a : b);
     final DateFormat formatter = DateFormat("dd.MM.yyyy");
     return newestDate != null ? formatter.format(newestDate) : null;
   }
 
-  String? getTheLatesUpdatedDateGuideX({
+  String? getTheLatesUpdatedDateGuide({
     List<Query$GetGuideDetails$FAQ_Types_by_id$questions?>? questions,
   }) {
-    final DateTime? newestDate = questions?.whereNonNull
-        .map((e) => e.FAQ_id!.date_updated!)
-        .reduce((a, b) => a.isAfter(b) ? a : b);
-
+    final newestDate = questions?.whereNonNull
+        .map((e) => e.FAQ_id?.date_updated)
+        .reduce((a, b) => (b == null || a != null && a.isAfter(b)) ? a : b);
     final DateFormat formatter = DateFormat("dd.MM.yyyy");
     return newestDate != null ? formatter.format(newestDate) : null;
   }
