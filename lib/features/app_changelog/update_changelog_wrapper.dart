efad1e79cc3192cc15a874f3149139fe2cef4e4e
Szymon Kowaliński <szymon@kowalinski.dev>
2024-10-27 18:52:21 +0100

    fix(changelog): hook on init crash

--- lib/features/app_changelog/update_changelog_wrapper.dart
+++ lib/features/app_changelog/update_changelog_wrapper.dart
@@ -1,5 +1,3 @@
-import "dart:async";
-
 import "package:flutter/material.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 
@@ -17,11 +15,11 @@ class UpdateChangelogWrapper extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     useEffectOnInit(() {
-      print("Wykonuje sie");
-      unawaited(showChangelog(context, ref));
+      Future.microtask(
+        () async => context.mounted ? showChangelog(context, ref) : null,
+      );
       return null;
     });
-
     return child;
   }
 }
