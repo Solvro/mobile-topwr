e1910cba43ccfc86f9adeb519e04ce29d5dbffd4
piwniczak2137 <70859223+thesun901@users.noreply.github.com>
2024-11-20 16:21:48 +0100

    feat: add interval refresh on the sks people counter

--- lib/features/sks_people_live/data/repository/latest_sks_user_data_repo.dart
+++ lib/features/sks_people_live/data/repository/latest_sks_user_data_repo.dart
@@ -1,3 +1,4 @@
+import "dart:async";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 import "../../../../shared_api_clients/sks_api_client.dart";
@@ -5,10 +6,29 @@ import "../models/sks_user_data.dart";
 
 part "latest_sks_user_data_repo.g.dart";
 
+extension RefIntervalRefreshX on Ref {
+  void setRefresh(Duration interval) {
+    final timer = Timer.periodic(
+      interval,
+      (t) => invalidateSelf(),
+    );
+    onDispose(timer.cancel);
+  }
+}
+
 @riverpod
 Future<SksUserData> getLatestSksUserData(Ref ref) async {
   final dio = ref.read(sksClientProvider);
-  const latestDataEndpoint = "sks-users/current/";
+  const latestDataEndpoint = "/sks-users/current/";
   final response = await dio.get(latestDataEndpoint);
-  return SksUserData.fromJson(response.data as Map<String, dynamic>);
+  final sksData = SksUserData.fromJson(response.data as Map<String, dynamic>);
+
+  final currentTime = DateTime.now();
+  final sksRefreshInterval = sksData.isResultRecent
+      ? sksData.nextUpdateTimestamp.difference(currentTime)
+      : const Duration(minutes: 1);
+
+  ref.setRefresh(sksRefreshInterval);
+
+  return sksData;
 }
