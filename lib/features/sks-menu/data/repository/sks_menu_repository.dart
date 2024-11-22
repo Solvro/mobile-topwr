ea1d755b03ee0e4250af9de40512b4ef51ec04ba
24bartixx <jestgitbartix@gmail.com>
2024-11-22 18:44:27 +0100

    chore: sks update url and adjust models

--- lib/features/sks-menu/data/repository/sks_menu_repository.dart
+++ lib/features/sks-menu/data/repository/sks_menu_repository.dart
@@ -6,27 +6,21 @@ import "package:riverpod_annotation/riverpod_annotation.dart";
 import "../../../../shared_api_clients/sks_api_client.dart";
 import "../models/dish_category_enum.dart";
 import "../models/sks_menu_data.dart";
+import "../models/sks_menu_response.dart";
 
 part "sks_menu_repository.g.dart";
 
 @riverpod
 Future<IMap<DishCategory, List<SksMenuDish>>> getSksMenuData(Ref ref) async {
 
-  final dio = ref.read(sksClientProvider);
   const mealsEndpoint = "/api/v1/meals/current";
+
+  final dio = ref.read(sksClientProvider);
   final response = await dio.get(mealsEndpoint);
+  final SksMenuResponse sksMenuResponse = SksMenuResponse.fromJson(response.data as Map<String, dynamic>);
 
-  if(!response.data['isMenuOnline']) return <DishCategory, List<SksMenuDish>>{}.toIMap();
-
-  final List<SksMenuDish> sksMenuDishesList = (response.data["meals"] as List<dynamic>)
-      .map((dish) => SksMenuDish.fromJson(dish as Map<String, dynamic>))
-      .toList()
-      ..sort((a, b) => a.category.index.compareTo(b.category.index));
-
-  final Map<DishCategory, List<SksMenuDish>> groupedData = groupBy(
-    sksMenuDishesList,
-    (SksMenuDish data) => data.category,
-  );
-
-  return groupedData.toIMap();
+  return sksMenuResponse.isMenuOnline 
+    ? groupBy(sksMenuResponse.meals, (SksMenuDish data) => data.category).toIMap()
+    : <DishCategory, List<SksMenuDish>>{}.toIMap();
+  
 }
