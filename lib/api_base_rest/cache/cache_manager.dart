a9ca5b3d7d96f3d04086b27211478dd3e22625b6
Tomasz Trela <153635094+tomasz-trela@users.noreply.github.com>
2024-11-23 20:27:57 +0100

    feat: caching_rest_response

---
+++ lib/api_base_rest/cache/cache_manager.dart
@@ -0,0 +1,22 @@
+import "package:flutter_cache_manager/flutter_cache_manager.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:riverpod_annotation/riverpod_annotation.dart";
+
+part "cache_manager.g.dart";
+
+@Riverpod(keepAlive: true)
+CacheManager cacheManagerSingleton(Ref ref) {
+  return CacheManager(
+    CacheManagerConfig.jsonCacheManagerConfig,
+  );
+}
+
+abstract class CacheManagerConfig {
+  static final jsonCacheManagerConfig = Config(
+    "JsonCacheManager7Days",
+    stalePeriod: const Duration(days: 7), 
+    maxNrOfCacheObjects: 20,
+  );
+
+  static const jsonExtesion = ".json";
+}
