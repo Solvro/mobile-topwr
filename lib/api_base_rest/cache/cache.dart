676a32e236e08ed595f0c1d097f054ed8f9fd3c0
Tomasz Trela <153635094+tomasz-trela@users.noreply.github.com>
2024-11-24 13:14:16 +0100

    feat: add custom rest caching framework (#426)
    * feat: caching_rest_response
    
    * chore/ format code
    
    * refactor/parametrize function
    
    * feat: condition
    
    * refactor/ meet review requirements
    
    * chore/ format code
    
    * feat: caching_rest_response
    
    * chore/ format code
    
    * refactor/parametrize function
    
    * feat: condition
    
    * refactor/ meet review requirements
    
    * chore/ format code
    
    * refactor/ unify rest clients
    
    * feat/ aditional comments and renaming

---
+++ lib/api_base_rest/cache/cache.dart
@@ -0,0 +1,37 @@
+import "dart:convert";
+import "dart:typed_data";
+
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "../client/dio_client.dart";
+import "cache_manager.dart";
+
+extension DataCachingX on Ref {
+  Future<T> getAndCacheData<T>(
+    String fullUrl,
+    int ttlDays,
+    T Function(Map<String, dynamic> json) fromJson,
+    bool Function() extraValidityCheck,
+  ) async {
+    final cacheManager = watch(restCacheManagerProvider(ttlDays));
+
+    final cachedFile = await cacheManager.getFileFromCache(fullUrl);
+    if (cachedFile != null && extraValidityCheck()) {
+      final cachedData = await cachedFile.file.readAsString();
+      final data = fromJson(
+        jsonDecode(cachedData) as Map<String, dynamic>,
+      );
+      return data;
+    }
+    final dio = watch(restClientProvider);
+    final response = await dio.get(fullUrl);
+    final sksData = fromJson(response.data as Map<String, dynamic>);
+
+    await cacheManager.putFile(
+      fullUrl,
+      Uint8List.fromList(jsonEncode(response.data).codeUnits),
+      fileExtension: CacheManagerConfig.jsonExtesion,
+    );
+
+    return sksData;
+  }
+}
