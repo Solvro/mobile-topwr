2001b7b07a4b3f6805500d4847f96c488137c0cc
Tomasz Trela <153635094+tomasz-trela@users.noreply.github.com>
2024-11-24 13:14:16 +0100

    feat: add custom rest caching framework (#426)
    * feat: caching_rest_response
    
    * chore/ format code
    
    * refactor/parametrize function
    
    * feat: condition
    
    * refactor/ meet review requirements
    
    * chore/ format code
    
    * feat: caching_rest_response
    
    * chore/ format code
    
    * refactor/parametrize function
    
    * feat: condition
    
    * refactor/ meet review requirements
    
    * chore/ format code
    
    * refactor/ unify rest clients
    
    * feat/ aditional comments and renaming

--- lib/features/sks-menu/data/repository/sks_menu_repository.dart
+++ lib/features/sks-menu/data/repository/sks_menu_repository.dart
@@ -3,7 +3,8 @@ import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
-import "../../../../shared_api_clients/sks_api_client.dart";
+import "../../../../api_base_rest/client/dio_client.dart";
+import "../../../../config/env.dart";
 import "../models/dish_category_enum.dart";
 import "../models/sks_menu_data.dart";
 import "../models/sks_menu_response.dart";
@@ -12,10 +13,10 @@ part "sks_menu_repository.g.dart";
 
 @riverpod
 Future<IMap<DishCategory, List<SksMenuDish>>> getSksMenuData(Ref ref) async {
-  const mealsEndpoint = "/meals/current";
+  final mealsUrl = "${Env.sksUrl}/meals/current";
 
-  final dio = ref.read(sksClientProvider);
-  final response = await dio.get(mealsEndpoint);
+  final dio = ref.read(restClientProvider);
+  final response = await dio.get(mealsUrl);
   final SksMenuResponse sksMenuResponse =
       SksMenuResponse.fromJson(response.data as Map<String, dynamic>);
 
