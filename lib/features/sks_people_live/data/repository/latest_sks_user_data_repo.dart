e50d3c89af0cc8f2daa7b8b7a54c93877943108f
Tomasz Trela <153635094+tomasz-trela@users.noreply.github.com>
2024-11-24 12:56:49 +0100

    refactor/ unify rest clients

--- lib/features/sks_people_live/data/repository/latest_sks_user_data_repo.dart
+++ lib/features/sks_people_live/data/repository/latest_sks_user_data_repo.dart
@@ -3,7 +3,8 @@ import "dart:async";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
-import "../../../../shared_api_clients/sks_api_client.dart";
+import "../../../../api_base_rest/client/dio_client.dart";
+import "../../../../config/env.dart";
 import "../../../../utils/ref_extensions.dart";
 import "../models/sks_user_data.dart";
 import "../sks_people_live_consts.dart";
@@ -12,8 +13,8 @@ part "latest_sks_user_data_repo.g.dart";
 
 @riverpod
 Future<SksUserData> getLatestSksUserData(Ref ref) async {
-  final dio = ref.read(sksClientProvider);
-  const latestDataEndpoint = "/sks-users/current/";
+  final dio = ref.watch(restClientProvider);
+  final latestDataEndpoint = "${Env.sksUrl}/sks-users/current/";
   final response = await dio.get(latestDataEndpoint);
   final sksData = SksUserData.fromJson(response.data as Map<String, dynamic>);
 
