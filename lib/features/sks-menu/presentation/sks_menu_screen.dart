4b5b407b0823cf8066c8b938a740d97914ba7fde
mikolaj-jalocha <mikics2k17@gmail.com>
2024-11-25 13:18:42 +0100

    fix: fix sorting order on sks menu screen to resembles one shown on sks website & adapt for new data format

--- lib/features/sks-menu/presentation/sks_menu_screen.dart
+++ lib/features/sks-menu/presentation/sks_menu_screen.dart
@@ -1,7 +1,6 @@
 import "dart:core";
 
 import "package:auto_route/annotations.dart";
-import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
@@ -9,8 +8,7 @@ import "../../../config/ui_config.dart";
 import "../../../utils/context_extensions.dart";
 import "../../../widgets/detail_views/detail_view_app_bar.dart";
 import "../../home_view/widgets/paddings.dart";
-import "../data/models/dish_category_enum.dart";
-import "../data/models/sks_menu_data.dart";
+import "../data/models/sks_menu_response.dart";
 import "../data/repository/sks_menu_repository.dart";
 import "widgets/sks_menu_data_source_link.dart";
 import "widgets/sks_menu_header.dart";
@@ -26,8 +24,14 @@ class SksMenuView extends ConsumerWidget {
 
     // TODO(mikolaj-jalocha): Add lottie animation on: error and when data is empty (sks's closed)
     return asyncSksMenuData.when(
-      data: (sksMenuData) =>
-          _SksMenuView(asyncSksMenuData.value ?? const IMap.empty()),
+      data: (sksMenuData) => _SksMenuView(
+        asyncSksMenuData.value ??
+            SksMenuResponse(
+              isMenuOnline: false,
+              lastUpdate: DateTime.now(),
+              meals: List.empty(),
+            ),
+      ),
       error: (error, stackTrace) => Scaffold(
         body: Center(
           child: Padding(
@@ -48,30 +52,31 @@ class SksMenuView extends ConsumerWidget {
 class _SksMenuView extends StatelessWidget {
   const _SksMenuView(this.sksMenuData);
 
-  final IMap<DishCategory, List<SksMenuDish>> sksMenuData;
+  final SksMenuResponse sksMenuData;
 
   @override
   Widget build(BuildContext context) {
-    if (sksMenuData.isEmpty) {
+    if (sksMenuData.meals.isEmpty) {
       return const Scaffold(
         body: Center(
           child: CircularProgressIndicator(),
         ),
       );
     }
-    final firstItem = sksMenuData.values.first.first;
     return Scaffold(
       appBar: DetailViewAppBar(context, title: context.localize.home_screen),
       body: ListView(
         children: [
           SksMenuHeader(
-            dateTimeOfLastUpdate: firstItem.updatedAt.toIso8601String(),
+            dateTimeOfLastUpdate: sksMenuData.lastUpdate.toIso8601String(),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(
               vertical: HomeViewConfig.paddingMedium,
             ),
-            child: MediumHorizontalPadding(child: SksMenuSection(sksMenuData)),
+            child: MediumHorizontalPadding(
+              child: SksMenuSection(sksMenuData.meals),
+            ),
           ),
           const SksMenuDataSourceLink(),
           const SizedBox(
--- lib/features/sks-menu/presentation/widgets/sks_menu_section.dart
+++ lib/features/sks-menu/presentation/widgets/sks_menu_section.dart
@@ -1,5 +1,7 @@
+import "package:collection/collection.dart";
 import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:flutter/cupertino.dart";
+
 import "../../data/models/dish_category_enum.dart";
 import "../../data/models/sks_menu_data.dart";
 import "sks_menu_tiles.dart";
@@ -7,15 +9,19 @@ import "sks_menu_tiles.dart";
 class SksMenuSection extends StatelessWidget {
   const SksMenuSection(this.data, {super.key});
 
-  final IMap<DishCategory, List<SksMenuDish>> data;
+  final List<SksMenuDish> data;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: data.keys.map((category) {
+      children: data
+          .map((dish) => dish.category)
+          .toISet()
+          .sorted((e1, e2) => e1.index.compareTo(e2.index))
+          .map((category) {
         return SksMenuTile(
           title: category.getLocalizedName(context),
-          dishes: data[category] ?? [],
+          dishes: data.where((e) => e.category == category).toList(),
         );
       }).toList(),
     );
