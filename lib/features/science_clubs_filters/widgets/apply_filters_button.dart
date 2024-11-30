402eba40d7d8e1eb7297017a5bb440f2b575c585
Bartosz Żurawski <92211556+BombardierBulge@users.noreply.github.com>
2024-11-30 17:43:06 +0100

    fix(science-clubs): shadows on apply filter button (#446)
    * /fix fix shadows on filter's screen
    
    * /fix fix shadows on filter's screen

--- lib/features/science_clubs_filters/widgets/apply_filters_button.dart
+++ lib/features/science_clubs_filters/widgets/apply_filters_button.dart
@@ -14,35 +14,38 @@ class ApplyFiltersButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: context.colorTheme.orangePomegranade,
-        elevation: 4,
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        shape: RoundedRectangleBorder(
-          borderRadius:
-              FilterConfig.radius, // Change this value to adjust the radius
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 2),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: context.colorTheme.orangePomegranade,
+          elevation: 4,
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          shape: RoundedRectangleBorder(
+            borderRadius:
+                FilterConfig.radius, // Change this value to adjust the radius
+          ),
         ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            width: 5,
-          ),
-          Text(
-            context.localize.apply,
-            style: context.textTheme.titleWhite,
-          ),
-          const SizedBox(
-            width: 5,
-          ),
-          Icon(
-            Icons.check,
-            color: context.colorTheme.whiteSoap,
-          ),
-        ],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              width: 5,
+            ),
+            Text(
+              context.localize.apply,
+              style: context.textTheme.titleWhite,
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Icon(
+              Icons.check,
+              color: context.colorTheme.whiteSoap,
+            ),
+          ],
+        ),
       ),
     );
   }
