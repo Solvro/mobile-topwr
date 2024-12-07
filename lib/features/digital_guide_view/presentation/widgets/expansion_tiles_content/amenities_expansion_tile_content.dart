401599c63582c48055e5d809cbe970a30d4f65d2
24bartixx <jestgitbartix@gmail.com>
2024-12-07 11:48:09 +0100

    refractor: skip method extraction in amenities expansion tile

--- lib/features/digital_guide_view/presentation/widgets/expansion_tiles_content/amenities_expansion_tile_content.dart
+++ lib/features/digital_guide_view/presentation/widgets/expansion_tiles_content/amenities_expansion_tile_content.dart
@@ -17,86 +17,53 @@ class AmenitiesExpansionTileContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ContactSection(
-      list: getIconsModels(context, digitalGuideResponse),
+      list: [
+        if (digitalGuideResponse.canAssistanceDog) 
+          ContactIconsModel(
+            text: context.localize.assistance_dog,
+            icon: Assets.svg.contactIcons.compass,
+          ),
+        if (digitalGuideResponse.isInductionLoop) 
+          ContactIconsModel(
+            text: context.localize.induction_loop,
+            icon: Assets.svg.contactIcons.compass,
+          ),
+        if (digitalGuideResponse.isMicroNavigationSystem) 
+          ContactIconsModel(
+            text: context.localize.micronavigation_system,
+            icon: Assets.svg.contactIcons.compass,
+          ),
+
+        if (digitalGuideResponse.areGuidancePaths) 
+          ContactIconsModel(
+            text: context.localize.guidance_paths,
+            icon: Assets.svg.contactIcons.compass,
+          ),
+
+        if (digitalGuideResponse.areBrailleBoards)
+          ContactIconsModel(
+            text: context.localize.information_boards_with_braille_description,
+            icon: Assets.svg.contactIcons.compass,
+          ),
+
+        if (digitalGuideResponse.areLargeFontBoards)
+          ContactIconsModel(
+            text: context.localize.information_boards_with_large_font,
+            icon: Assets.svg.contactIcons.compass,
+          ),
+
+        if (digitalGuideResponse.isSignLanguageInterpreter)
+          ContactIconsModel(
+            text: context.localize.sign_language_interpreter,
+            icon: Assets.svg.contactIcons.compass,
+          ),
+
+        if (digitalGuideResponse.areEmergencyChairs)
+          ContactIconsModel(
+            text: context.localize.emergency_chairs,
+            icon: Assets.svg.contactIcons.compass,
+          ),
+      ].lock,
     );
   }
-}
-
-IList<ContactIconsModel> getIconsModels(
-    BuildContext context, DigitalGuideResponse digitalGuideResponse) {
-  IList<ContactIconsModel> iconsModels = IList<ContactIconsModel>();
-
-  if (digitalGuideResponse.canAssistanceDog) {
-    iconsModels = iconsModels.add(
-      ContactIconsModel(
-        text: context.localize.assistance_dog,
-        icon: Assets.svg.contactIcons.compass,
-      ),
-    );
-  }
-
-  if (digitalGuideResponse.isInductionLoop) {
-    iconsModels = iconsModels.add(
-      ContactIconsModel(
-        text: context.localize.induction_loop,
-        icon: Assets.svg.contactIcons.compass,
-      ),
-    );
-  }
-
-  if (digitalGuideResponse.isMicroNavigationSystem) {
-    iconsModels = iconsModels.add(
-      ContactIconsModel(
-        text: context.localize.micronavigation_system,
-        icon: Assets.svg.contactIcons.compass,
-      ),
-    );
-  }
-
-  if (digitalGuideResponse.areGuidancePaths) {
-    iconsModels = iconsModels.add(
-      ContactIconsModel(
-        text: context.localize.guidance_paths,
-        icon: Assets.svg.contactIcons.compass,
-      ),
-    );
-  }
-
-  if (digitalGuideResponse.areBrailleBoards) {
-    iconsModels = iconsModels.add(
-      ContactIconsModel(
-        text: context.localize.information_boards_with_braille_description,
-        icon: Assets.svg.contactIcons.compass,
-      ),
-    );
-  }
-
-  if (digitalGuideResponse.areLargeFontBoards) {
-    iconsModels = iconsModels.add(
-      ContactIconsModel(
-        text: context.localize.information_boards_with_large_font,
-        icon: Assets.svg.contactIcons.compass,
-      ),
-    );
-  }
-
-  if (digitalGuideResponse.isSignLanguageInterpreter) {
-    iconsModels = iconsModels.add(
-      ContactIconsModel(
-        text: context.localize.sign_language_interpreter,
-        icon: Assets.svg.contactIcons.compass,
-      ),
-    );
-  }
-
-  if (digitalGuideResponse.areEmergencyChairs) {
-    iconsModels = iconsModels.add(
-      ContactIconsModel(
-        text: context.localize.emergency_chairs,
-        icon: Assets.svg.contactIcons.compass,
-      ),
-    );
-  }
-
-  return iconsModels;
 }
