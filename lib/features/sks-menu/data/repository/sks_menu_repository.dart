3736a3df3210ab8adddfbb58b5f3646adabdeb90
Szymon Kowaliński <szymon@kowalinski.dev>
2024-12-09 23:23:31 +0100

    feat(sks-menu): add rest caching

--- lib/features/sks-menu/data/repository/sks_menu_repository.dart
+++ lib/features/sks-menu/data/repository/sks_menu_repository.dart
@@ -1,8 +1,9 @@
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
-import "../../../../api_base_rest/client/dio_client.dart";
+import "../../../../api_base_rest/cache/cache.dart";
 import "../../../../config/env.dart";
+import "../../../../utils/datetime_utils.dart";
 import "../models/dish_category_enum.dart";
 import "../models/sks_menu_response.dart";
 
@@ -11,15 +12,20 @@ part "sks_menu_repository.g.dart";
 @riverpod
 Future<ExtendedSksMenuResponse> getSksMenuData(Ref ref) async {
   final mealsUrl = "${Env.sksUrl}/meals/current";
+  const ttlDays = 1;
 
-  final dio = ref.read(restClientProvider);
-  final response = await dio.get(mealsUrl);
-  final SksMenuResponse sksMenuResponse =
-      SksMenuResponse.fromJson(response.data as Map<String, dynamic>);
+  final sksMenuResponse = await ref.getAndCacheData(
+    mealsUrl,
+    ttlDays,
+    SksMenuResponse.fromJson,
+    extraValidityCheck: (data) =>
+        data.isMenuOnline && DateTime.now().date == data.lastUpdate.date,
+  );
 
   final trueMeals = sksMenuResponse.meals
       .where((e) => e.category != DishCategory.technicalInfo)
       .toList();
+
   final technicalInfos = sksMenuResponse.meals
       .where((e) => e.category == DishCategory.technicalInfo)
       .map((e) => e.name)
