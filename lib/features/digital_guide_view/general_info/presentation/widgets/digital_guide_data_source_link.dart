de3a96980341cf612595630e62f07f65184de52e
24bartixx <jestgitbartix@gmail.com>
2024-12-11 11:47:04 +0100

    fix: add horizontal padding

--- lib/features/digital_guide_view/general_info/presentation/widgets/digital_guide_data_source_link.dart
+++ lib/features/digital_guide_view/general_info/presentation/widgets/digital_guide_data_source_link.dart
@@ -1,32 +1,36 @@
 import "package:flutter/widgets.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
+import "../../../../../config/ui_config.dart";
 import "../../../../../theme/app_theme.dart";
 import "../../../../../utils/context_extensions.dart";
 
 class DigitalGuideDataSourceLink extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Text.rich(
-      TextSpan(
-        text: "${context.localize.data_come_from_website}: ",
-        style: const TextStyle(
-          fontWeight: FontWeight.bold,
-        ),
-        children: [
-          TextSpan(
-            text: context.localize.digital_guide_website,
-            style: context.textTheme.bodyOrange.copyWith(
-              decoration: TextDecoration.underline,
-              decorationColor: context.colorTheme.orangePomegranade,
-              fontWeight: FontWeight.bold,
-            ),
-            // TODO(Bartosh): on tap url handling -> webbrowser launch
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: DigitalGuideConfig.paddingMedium),
+      child: Text.rich(
+        TextSpan(
+          text: "${context.localize.data_come_from_website}: ",
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
           ),
-        ],
+          children: [
+            TextSpan(
+              text: context.localize.digital_guide_website,
+              style: context.textTheme.bodyOrange.copyWith(
+                decoration: TextDecoration.underline,
+                decorationColor: context.colorTheme.orangePomegranade,
+                fontWeight: FontWeight.bold,
+              ),
+              // TODO(Bartosh): on tap url handling -> webbrowser launch
+            ),
+          ],
+        ),
+        textAlign: TextAlign.center,
+        style: context.textTheme.body,
       ),
-      textAlign: TextAlign.center,
-      style: context.textTheme.body,
     );
   }
 }
