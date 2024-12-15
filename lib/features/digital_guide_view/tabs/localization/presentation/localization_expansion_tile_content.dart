904099dcb74cc5a306de0c7d9eda25c5bae78f9b
majam <majamroczek.pl@gmail.com>
2024-12-15 16:03:16 +0100

    feat: add location map

--- lib/features/digital_guide_view/tabs/localization/presentation/localization_expansion_tile_content.dart
+++ lib/features/digital_guide_view/tabs/localization/presentation/localization_expansion_tile_content.dart
@@ -1,8 +1,80 @@
-import "package:flutter/widgets.dart";
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 
-class LocalizationExpansionTileContent extends StatelessWidget {
+import "../../../../../theme/app_theme.dart";
+import "../../../../../utils/context_extensions.dart";
+import "../../../../widgets/my_error_widget.dart";
+import "../../../parkings_view/widgets/parkings_icons.icons.dart";
+import "../data/repository/localization_repository.dart";
+
+class LocalizationExpansionTileContent extends ConsumerWidget {
+  const LocalizationExpansionTileContent({
+    super.key,
+    required this.buildingId,
+  });
+
+  final int buildingId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncImageUrl =
+        ref.watch(getImageUrlFromBuildingProvider(buildingId));
+
+    return asyncImageUrl.when(
+      data: (imageUrl) => _LocalizationExpansionTileContent(imageUrl: imageUrl),
+      error: (error, stackTrace) => MyErrorWidget(error),
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
+
+class _LocalizationExpansionTileContent extends StatelessWidget {
+  const _LocalizationExpansionTileContent({
+    required this.imageUrl,
+  });
+
+  final String? imageUrl;
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Lorem ipsum text siuuuu Cristiano Rolando");
+    return Column(
+      children: [
+        if (imageUrl != null)
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.network(
+                imageUrl!,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) =>
+                    const Center(child: Text("Cannot load photo")),
+              ),
+            ),
+          )
+        else
+          const Center(child: Text("Photo not available")),
+        TextButton.icon(
+          icon: Icon(
+            ParkingsIcons.map_nav,
+            color: context.colorTheme.orangePomegranade,
+            size: 16,
+          ),
+          onPressed: () {},
+          label: Text.rich(
+            TextSpan(
+              text: context.localize.navigate_to_building,
+              style: context.textTheme.bodyOrange.copyWith(
+                decoration: TextDecoration.underline,
+                decorationColor: context.colorTheme.orangePomegranade,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
