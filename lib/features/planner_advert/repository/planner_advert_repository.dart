27dbab415d68bf5a27e2b7060ba2dc55cc83cb35
mikolaj-jalocha <mikics2k17@gmail.com>
2025-01-25 20:24:19 +0100

    feat(planner-advert): setup data layer

---
+++ lib/features/planner_advert/repository/planner_advert_repository.dart
@@ -0,0 +1,20 @@
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:riverpod_annotation/riverpod_annotation.dart";
+
+import "../../../api_base/query_adapter.dart";
+import "../../../config/ttl_config.dart";
+import "getPlannerAdvertContent.graphql.dart";
+
+part "planner_advert_repository.g.dart";
+
+typedef PlannerAdvertContent = Query$GetPlannerAdvertContent$PlannerAdvert;
+
+@riverpod
+Future<PlannerAdvertContent?> plannerAdvertContentRepository(Ref ref) async {
+  final results = await ref.queryGraphql(
+    Options$Query$GetPlannerAdvertContent(),
+    TtlKey.plannerAdvertRepository,
+  );
+
+  return results?.PlannerAdvert;
+}
