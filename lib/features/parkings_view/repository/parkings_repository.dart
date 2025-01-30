9f02ce5ab1bc0c2e7e7421ff0bfec5c365835488
tomasz-trela <trela.tomasz@protonmail.com>
2025-01-30 15:51:17 +0100

    feat: use service in repo

--- lib/features/parkings_view/repository/parkings_repository.dart
+++ lib/features/parkings_view/repository/parkings_repository.dart
@@ -5,24 +5,33 @@ import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
+import "../../../api_base/gql_client_provider.dart";
 import "../../../utils/ref_extensions.dart";
 import "../api_client/iparking_client.dart";
 import "../api_client/iparking_commands.dart";
 import "../models/parking.dart";
+import "../pick_correct_api_service/getUseParkingApiWrapper.graphql.dart";
 import "local_fav_parking_repository.dart";
 
 part "parkings_repository.g.dart";
 
 @riverpod
 Future<IList<Parking>> parkingsRepository(Ref ref) async {
-  ref.setRefresh(ParkingsConfig.parkingsRefreshInterval);
-  final response = await ref.postIParkingCommand<Map<String, dynamic>>(
-    FetchPlacesCommand(DateTime.now()),
-  );
-  final parkings = response.data?["places"] as List<dynamic>;
-  final list = parkings.whereType<Map<String, dynamic>>().map(Parking.fromJson);
+  final apiClient = await ref.watch(grapqlClientProvider.future);
+  final response = await apiClient.query$GetUseParkingApiWrapper();
+  final useParkingApiWrapper = response.parsedData?.CacheReferenceNumber?.useParkingApiWrapper ?? false;
 
-  return _sortParkingsByFav(list, ref).toIList();
+  if(!useParkingApiWrapper){
+    ref.setRefresh(ParkingsConfig.parkingsRefreshInterval);
+    final response = await ref.postIParkingCommand<Map<String, dynamic>>(
+      FetchPlacesCommand(DateTime.now()),
+    );
+    final parkings = response.data?["places"] as List<dynamic>;
+    final list = parkings.whereType<Map<String, dynamic>>().map(Parking.fromJson);
+    return _sortParkingsByFav(list, ref).toIList();
+  }
+
+  return IList.empty();
 }
 
 DoubleLinkedQueue<Parking> _sortParkingsByFav(
