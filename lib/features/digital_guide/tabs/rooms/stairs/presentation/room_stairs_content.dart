a29fb70261eb19274bd17e870645471aa6c59880
piwniczak2137 <70859223+thesun901@users.noreply.github.com>
2025-02-19 18:52:46 +0100

    feat: add schody section

---
+++ lib/features/digital_guide/tabs/rooms/stairs/presentation/room_stairs_content.dart
@@ -0,0 +1,65 @@
+import "package:fast_immutable_collections/fast_immutable_collections.dart";
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+import "../../../../../../config/ui_config.dart";
+import "../../../../../../gen/assets.gen.dart";
+import "../../../../../../widgets/my_error_widget.dart";
+import "../../../../presentation/widgets/accessibility_profile_card.dart";
+import "../../../../presentation/widgets/bullet_list.dart";
+import "../models/room_stairs_response.dart";
+import "../repository/room_stairs_repository.dart";
+import "../utils/room_stairs_comment_list.dart";
+
+class RoomStairsContent extends ConsumerWidget {
+  const RoomStairsContent({required this.roomStairsId});
+
+  final int roomStairsId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncRoomStairs =
+        ref.watch(roomStairsRepositoryProvider(roomStairsId));
+
+    return asyncRoomStairs.when(
+      data: (roomStairsData) => _RoomStairsContent(
+        roomStairsResponse: roomStairsData,
+      ),
+      error: (error, stackTrace) => MyErrorWidget(error),
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
+
+class _RoomStairsContent extends ConsumerWidget {
+  const _RoomStairsContent({required this.roomStairsResponse});
+
+  final RoomStairsResponse roomStairsResponse;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final IList<String> roomStairsComments =
+        context.getRoomStairsCommentsList(roomStairsResponse);
+
+    final IList<String> roomStairsAccessibilityComments =
+        context.getRoomStairsAccessibilityCommentsList(roomStairsResponse);
+
+    return Padding(
+      padding: DigitalGuideConfig.symetricalPaddingBig.copyWith(top: 0),
+      child: Column(
+        children: [
+          BulletList(items: roomStairsComments),
+          const SizedBox(
+            height: DigitalGuideConfig.heightBig,
+          ),
+          AccessibilityProfileCard(
+            items: roomStairsAccessibilityComments,
+            icon: Assets.svg.digitalGuide.accessibilityAlerts.blindProfile,
+          ),
+        ],
+      ),
+    );
+  }
+}
