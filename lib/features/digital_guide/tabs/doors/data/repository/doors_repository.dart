09efed17685e73480bbae91b663ecffd3fb2a496
Bartosh <101900992+24bartixx@users.noreply.github.com>
2025-02-20 17:07:03 +0100

    feat(digital-guide): add Doors (#579)
    * feat: digital guide doors
    
    * chore: format
    
    * chore: format

---
+++ lib/features/digital_guide/tabs/doors/data/repository/doors_repository.dart
@@ -0,0 +1,27 @@
+import "package:fast_immutable_collections/fast_immutable_collections.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:riverpod_annotation/riverpod_annotation.dart";
+
+import "../../../../data/api/digital_guide_get_and_cache.dart";
+import "../model/door.dart";
+
+part "doors_repository.g.dart";
+
+@riverpod
+Future<Door> doorsRepository(Ref ref, int doorsID) async {
+  return ref.getAndCacheDataFromDigitalGuide(
+    "doors/$doorsID",
+    Door.fromJson,
+    onRetry: () => ref.invalidateSelf(),
+  );
+}
+
+@riverpod
+Future<IList<Door>> doorsListRepository(
+  Ref ref,
+  List<int> ids,
+) async {
+  Future<Door> getDoor(int id) => ref.watch(doorsRepositoryProvider(id).future);
+  final doors = await Future.wait(ids.map(getDoor));
+  return doors.lock;
+}
---
+++ lib/features/digital_guide/tabs/doors/presentation/door_view.dart
@@ -0,0 +1,105 @@
+import "package:auto_route/annotations.dart";
+import "package:fast_immutable_collections/fast_immutable_collections.dart";
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+import "../../../../../config/ui_config.dart";
+import "../../../../../theme/app_theme.dart";
+import "../../../../../utils/context_extensions.dart";
+import "../../../../../widgets/detail_views/detail_view_app_bar.dart";
+import "../../../../../widgets/horizontal_symmetric_safe_area.dart";
+import "../../../../../widgets/my_error_widget.dart";
+import "../../../presentation/widgets/bullet_list.dart";
+import "../../../presentation/widgets/digital_guide_loading_view.dart";
+import "../data/model/door.dart";
+import "../data/repository/doors_repository.dart";
+
+@RoutePage()
+class DoorView extends ConsumerWidget {
+  const DoorView({
+    required this.doorsID,
+  });
+
+  final int doorsID;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncDoors = ref.watch(doorsRepositoryProvider(doorsID));
+
+    return asyncDoors.when(
+      data: (data) => _DoorsView(
+        door: data,
+      ),
+      error: (
+        error,
+        stackTrace,
+      ) {
+        return HorizontalSymmetricSafeAreaScaffold(
+          appBar: DetailViewAppBar(),
+          body: MyErrorWidget(error),
+        );
+      },
+      loading: () {
+        return const DigitalGuideLoadingView();
+      },
+    );
+  }
+}
+
+class _DoorsView extends ConsumerWidget {
+  const _DoorsView({required this.door});
+
+  final Door door;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final IList<String> textStrings = [
+      if (door.translations.pl.comment.isNotEmpty) door.translations.pl.comment,
+      door.translations.pl.fromTo,
+      "${context.localize.door_visible_from_outside(door.isGoodDoorVisibleFromOutside)} ${door.translations.pl.isGoodDoorVisibleFromOutsideComment}",
+      "${context.localize.door_visible_from_inside(door.isGoodDoorVisibleFromInside)} ${door.translations.pl.isGoodDoorVisibleFromInsideComment}",
+      if (door.doorType == DoorType.singleLeafDoor)
+        context.localize.single_leaf_door
+      else if (door.doorType == DoorType.doubleLeafDoor)
+        context.localize.double_leaf_door
+      else if (door.doorType == DoorType.singleLeafDoor)
+        context.localize.single_leaf_door_sliding
+      else if (door.doorType == DoorType.doubleLeafDoorSliding)
+        context.localize.double_leaf_door_sliding
+      else if (door.doorType == DoorType.swingDoor)
+        context.localize.swing_door,
+      "${context.localize.main_wing_highlighted(door.isMainWingHighlighted)} ${door.translations.pl.isMainWingHighlightedComment}",
+      "${context.localize.increased_force_required(door.isIncreasedForceRequired)} ${door.translations.pl.isIncreasedForceRequiredComment}",
+      "${context.localize.door_closer(door.isDoorCloser)} ${door.translations.pl.isDoorCloserComment}",
+    ]
+        .map((string) => string.trim())
+        .where((string) => string.isNotEmpty)
+        .toIList();
+
+    return Scaffold(
+      appBar: DetailViewAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: DigitalGuideConfig.heightBig,
+        ),
+        child: CustomScrollView(
+          slivers: [
+            SliverList(
+              delegate: SliverChildListDelegate([
+                Text(
+                  context.localize.door,
+                  style: context.textTheme.headline
+                      .copyWith(fontSize: DigitalGuideConfig.headlineFont),
+                ),
+                const SizedBox(height: DigitalGuideConfig.heightMedium),
+                BulletList(
+                  items: textStrings,
+                ),
+              ]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
