b14fdca043632c07c833da25b1a4268263684827
tomasz-trela <trela.tomasz@protonmail.com>
2025-02-27 22:20:46 +0100

    feat: repo and models

---
+++ lib/features/digital_guide/tabs/information_points/models/digital_guide_information_points.dart
@@ -0,0 +1,68 @@
+import "package:freezed_annotation/freezed_annotation.dart";
+
+import "../../../../../utils/type_converter.dart";
+
+part "digital_guide_information_points.freezed.dart";
+part "digital_guide_information_points.g.dart";
+
+@freezed
+class DigitalGuideInformationPoint with _$DigitalGuideInformationPoint {
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  factory DigitalGuideInformationPoint({
+    required int id,
+    required DigitalGuideInformationPointTranslations translations,
+    required int orderNumber,
+    @JsonKey(fromJson: stringToBool) required bool isMarked,
+    @JsonKey(fromJson: stringToBool) required bool isVisibleFromAfar,
+    @JsonKey(fromJson: stringToBool) required bool areAccessBarriers,
+    @JsonKey(fromJson: stringToBool) required bool areSeatsAvailable,
+    @JsonKey(fromJson: stringToBool) required bool isMovementSpace,
+    @JsonKey(fromJson: stringToBool) required bool isGoodLit,
+    required int counterHeight,
+    @JsonKey(fromJson: stringToBool) required bool isSpaceUnderCounter,
+    @JsonKey(fromJson: stringToBool) required bool isSpaceOnCounter,
+    @JsonKey(fromJson: stringToBool) required bool hasInductionLoop,
+    required int building,
+    required List<dynamic> rings,
+    required List<dynamic> images,
+  }) = _DigitalGuideInformationPoint;
+
+  factory DigitalGuideInformationPoint.fromJson(Map<String, dynamic> json) =>
+      _$DigitalGuideInformationPointFromJson(json);
+}
+
+@freezed
+class DigitalGuideInformationPointTranslations
+    with _$DigitalGuideInformationPointTranslations {
+  factory DigitalGuideInformationPointTranslations({
+    required DigitalGuideInformationPointTranslationDetails pl,
+  }) = _DigitalGuideInformationPointTranslations;
+
+  factory DigitalGuideInformationPointTranslations.fromJson(
+          Map<String, dynamic> json) =>
+      _$DigitalGuideInformationPointTranslationsFromJson(json);
+}
+
+@freezed
+class DigitalGuideInformationPointTranslationDetails
+    with _$DigitalGuideInformationPointTranslationDetails {
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  factory DigitalGuideInformationPointTranslationDetails({
+    required String location,
+    required String isMarkedComment,
+    required String isVisibleFromAfarComment,
+    required String areAccessBarriersComment,
+    required String areSeatsAvailableComment,
+    required String isMovementSpaceComment,
+    required String isGoodLitComment,
+    required String isSpaceUnderCounterComment,
+    required String isSpaceOnCounterComment,
+    required String hasInductionLoopComment,
+    required String workingDaysAndHours,
+    required String comment,
+  }) = _DigitalGuideInformationPointTranslationDetails;
+
+  factory DigitalGuideInformationPointTranslationDetails.fromJson(
+          Map<String, dynamic> json) =>
+      _$DigitalGuideInformationPointTranslationDetailsFromJson(json);
+}
