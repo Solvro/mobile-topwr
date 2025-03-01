d7550981f697d3fe344d28ef03df8e180f903a32
piwniczak2137 <70859223+thesun901@users.noreply.github.com>
2025-03-01 18:32:40 +0100

    feat: add room accessibility

--- lib/features/digital_guide/tabs/rooms/stairs/presentation/room_stairs_content.dart
+++ lib/features/digital_guide/tabs/rooms/stairs/presentation/room_stairs_content.dart
@@ -1,12 +1,15 @@
+import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
 import "../../../../../../config/ui_config.dart";
+import "../../../../../../utils/context_extensions.dart";
 import "../../../../../../widgets/my_error_widget.dart";
+import "../../../../presentation/widgets/accessibility_profile_card.dart";
 import "../../../../presentation/widgets/bullet_list.dart";
+import "../business/room_stairs_accessibility_comments_manager.dart";
 import "../models/room_stairs_response.dart";
 import "../repository/room_stairs_repository.dart";
-import "../utils/room_stairs_comment_list.dart";
 
 class RoomStairsContent extends ConsumerWidget {
   const RoomStairsContent({required this.roomStairsId});
@@ -32,12 +35,29 @@ class _RoomStairsContent extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final roomStairsComments = context.getRoomStairsCommentsList(roomStairsResponse);
+    final comments = roomStairsResponse.translations.pl;
+
+    final roomStairsComments = [
+      roomStairsResponse.translations.pl.location,
+      "${context.localize.room_stairs_is_nonslip_surface(
+        roomStairsResponse.isNonslipSurface.toLowerCase(),
+      )} ${comments.isNonslipSurfaceComment}",
+    ].lock;
+
 
     return Padding(
       padding: DigitalGuideConfig.symetricalPaddingBig.copyWith(top: 0),
       child: Column(
-        children: [BulletList(items: roomStairsComments), const SizedBox(height: DigitalGuideConfig.heightBig)],
+        children: [
+          BulletList(items: roomStairsComments),
+          AccessibilityProfileCard(
+            accessibilityCommentsManager:
+                RoomStairsAccessibilityCommentsManager(
+              roomStairsResponse: roomStairsResponse,
+              l10n: context.localize,
+            ),
+          ),
+        ],
       ),
     );
   }
