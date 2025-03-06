19267d96b1e4386f3708d4efba11b7aad64763f2
Bartosh <101900992+24bartixx@users.noreply.github.com>
2025-03-06 09:31:44 +0100

    feat(digital-guide): add lodge accessibility notes (#631)
    * feat: lodge accessibility motor impairment only
    
    * feat: rest of disabilities in the lodge accessibility
    
    * chore: format
    
    * refactor: remove unnecessary not null assertation
    
    * refactor: use IList.empty()

--- lib/features/digital_guide/tabs/lodge/data/models/digital_guide_lodge.dart
+++ lib/features/digital_guide/tabs/lodge/data/models/digital_guide_lodge.dart
@@ -5,9 +5,16 @@ part "digital_guide_lodge.g.dart";
 
 @freezed
 abstract class DigitalGuideLodge with _$DigitalGuideLodge {
+  @JsonSerializable(fieldRename: FieldRename.snake)
   const factory DigitalGuideLodge({
     required int id,
     required DigitalGuideTranslationsLodge translations,
+    required String areAccessBarriers,
+    required String isMovementSpace,
+    required double counterHeight,
+    required String isSpaceUnderCounter,
+    required String isVisibleFromAfar,
+    required String isGoodLit,
     @JsonKey(name: "images") required List<int>? imagesIds,
   }) = _DigitalGuideLodge;
 
@@ -30,6 +37,11 @@ abstract class DigitalGuideTranslationLodge with _$DigitalGuideTranslationLodge 
     required String location,
     required String workingDaysAndHours,
     required String comment,
+    required String areAccessBarriersComment,
+    required String isMovementSpaceComment,
+    required String isSpaceUnderCounterComment,
+    required String isVisibleFromAfarComment,
+    required String isGoodLitComment,
   }) = _DigitalGuideTranslationLodge;
   factory DigitalGuideTranslationLodge.fromJson(Map<String, dynamic> json) =>
       _$DigitalGuideTranslationLodgeFromJson(json);
