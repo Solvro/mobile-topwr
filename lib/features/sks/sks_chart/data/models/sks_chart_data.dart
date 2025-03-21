6add099a6a29206cfd37e11cb4561fe5bc99c25f
24bartixx <jestgitbartix@gmail.com>
2025-03-21 17:35:46 +0100

    fix: do not display red numbers one hour forward while holding

--- lib/features/sks/sks_chart/data/models/sks_chart_data.dart
+++ lib/features/sks/sks_chart/data/models/sks_chart_data.dart
@@ -2,6 +2,8 @@ import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:flutter/foundation.dart";
 import "package:freezed_annotation/freezed_annotation.dart";
 
+import "../../../../../utils/datetime_utils.dart";
+
 part "sks_chart_data.freezed.dart";
 part "sks_chart_data.g.dart";
 
@@ -10,15 +12,15 @@ abstract class SksChartData with _$SksChartData {
   const factory SksChartData({
     required int activeUsers,
     required int movingAverage21,
-    @JsonKey(fromJson: toLocalPolish) required DateTime externalTimestamp,
+    @JsonKey(fromJson: fromJsonToLocalTime) required DateTime externalTimestamp,
   }) = _SksChartData;
 
   factory SksChartData.fromJson(Map<String, dynamic> json) => _$SksChartDataFromJson(json);
 }
 
-DateTime toLocalPolish(String dateTimeString) {
-  return DateTime.parse(dateTimeString).toUtc().toLocal();
-}
+// DateTime toLocalPolish(String dateTimeString) {
+//   return DateTime.parse(dateTimeString).toUtc().toLocal();
+// }
 
 extension SksChartDataIListX on IList<SksChartData> {
   double get maxNumberOfUsers {
