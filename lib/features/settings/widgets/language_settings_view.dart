11f65f7f57e396a7a2e052774a9dfb6025abc33b
BombardierBulge <z.bartosz.z@wp.pl>
2025-03-27 13:47:53 +0100

    fix: remove statefull builder

--- lib/features/settings/widgets/language_settings_view.dart
+++ lib/features/settings/widgets/language_settings_view.dart
@@ -15,50 +15,48 @@ class LanguageDialog extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final selectedLanguage = context.localize.localeName;
+    final languages = [
+      (context.localize.polish_id, context.localize.polish_title),
+      (context.localize.english_id, context.localize.english_title),
+    ];
     return AlertDialog(
       backgroundColor: context.colorTheme.whiteSoap,
       contentPadding: const EdgeInsets.all(AboutUsConfig.spacerHeight),
-      content: StatefulBuilder(
-        builder: (context, setState) {
-          final languages = [(context.localize.polish_id, context.localize.polish_title), (context.localize.english_id, context.localize.english_title)];
-
-          return SizedBox(
-            width: double.maxFinite,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  context.localize.language,
-                  style: context.textTheme.headline.copyWith(fontSize: AboutUsConfig.dialogTitleFontSize),
-                ),
-                const SizedBox(height: AboutUsConfig.spacerHeight),
-                ListView.separated(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: languages.length,
-                  itemBuilder: (context, index) {
-                    final (code, name) = languages[index];
-                    final selected = selectedLanguage == code;
-                    return DecoratedBox(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(NavigationTabViewConfig.radius),
-                        color: context.colorTheme.greyLight,
-                      ),
-                      child: ListTile(
-                        title: Text(name),
-                        trailing: selected ? Icon(Icons.check, color: context.colorTheme.orangePomegranade) : null,
-                        onTap: () {
-                          Navigator.pop(context, code);
-                        },
-                      ),
-                    );
-                  },
-                  separatorBuilder: (context, index) => const SizedBox(height: AboutUsConfig.spacerHeight),
-                ),
-              ],
+      content: SizedBox(
+        width: double.maxFinite,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              context.localize.language,
+              style: context.textTheme.headline.copyWith(fontSize: AboutUsConfig.dialogTitleFontSize),
             ),
-          );
-        },
+            const SizedBox(height: AboutUsConfig.spacerHeight),
+            ListView.separated(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: languages.length,
+              itemBuilder: (context, index) {
+                final (code, name) = languages[index];
+                final selected = selectedLanguage == code;
+                return DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(NavigationTabViewConfig.radius),
+                    color: context.colorTheme.greyLight,
+                  ),
+                  child: ListTile(
+                    title: Text(name),
+                    trailing: selected ? Icon(Icons.check, color: context.colorTheme.orangePomegranade) : null,
+                    onTap: () {
+                      Navigator.pop(context, code);
+                    },
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(height: AboutUsConfig.spacerHeight),
+            ),
+          ],
+        ),
       ),
     );
   }
