7bba7d32ff5424fb4b4da52d98402413b7f89ac3
Bartosz Żurawski <92211556+BombardierBulge@users.noreply.github.com>
2025-03-28 14:29:36 +0100

    feat: add possibility for the user to change app's language (#648)
    * Add lenguage panel in settings view
    
    * typo fix
    
    * feat: add language selection view
    
    * feat: add language emoji in selection menu
    
    * fix: fix language emoji position in selection menu and code optimization
    
    * feat: language change implementation and small fixes
    
    * fix: removed hardcoded value
    
    * fix: remove statefull builder

---
+++ lib/features/settings/settings.dart
@@ -0,0 +1,50 @@
+import "package:auto_route/auto_route.dart";
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+import "../../../../../widgets/detail_views/detail_view_app_bar.dart";
+import "../../../../config/ui_config.dart";
+import "../../../../theme/app_theme.dart";
+import "../../../../utils/context_extensions.dart";
+import "../../services/translations_service/business/translations_notifier.dart";
+import "../../services/translations_service/data/models/supported_languages.dart";
+import "../navigation_tab_view/widgets/navigation_tile.dart";
+import "widgets/language_settings_view.dart";
+
+@RoutePage()
+class SettingsView extends ConsumerWidget {
+  const SettingsView();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final widgets = [
+      Text(context.localize.settings, style: context.textTheme.headline),
+      NavigationTile(
+        onTap: () async {
+          final selectedLang = await LanguageDialog.show(context);
+          if (selectedLang != null) {
+            final supportedLocale = SupportedLocales.fromString(selectedLang);
+            await ref.read(translationsProvider.notifier).setLocale(supportedLocale);
+          }
+        },
+        title: context.localize.language,
+        icon: Icons.speaker_notes_rounded,
+      ),
+    ];
+
+    return Scaffold(
+      backgroundColor: context.colorTheme.whiteSoap,
+      appBar: DetailViewAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(AboutUsConfig.spacerHeight),
+        child: ListView.separated(
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: widgets.length,
+          itemBuilder: (context, index) => widgets[index],
+          separatorBuilder: (context, index) => const SizedBox(height: AboutUsConfig.spacerHeight),
+          shrinkWrap: true,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/settings/widgets/language_settings_view.dart
@@ -0,0 +1,63 @@
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+import "../../../../config/ui_config.dart";
+import "../../../../theme/app_theme.dart";
+import "../../../utils/context_extensions.dart";
+
+class LanguageDialog extends ConsumerWidget {
+  const LanguageDialog();
+
+  static Future<String?> show(BuildContext context) async {
+    return showDialog<String>(context: context, builder: (context) => const LanguageDialog());
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedLanguage = context.localize.localeName;
+    final languages = [
+      (context.localize.polish_id, context.localize.polish_title),
+      (context.localize.english_id, context.localize.english_title),
+    ];
+    return AlertDialog(
+      backgroundColor: context.colorTheme.whiteSoap,
+      contentPadding: const EdgeInsets.all(AboutUsConfig.spacerHeight),
+      content: SizedBox(
+        width: double.maxFinite,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              context.localize.language,
+              style: context.textTheme.headline.copyWith(fontSize: AboutUsConfig.dialogTitleFontSize),
+            ),
+            const SizedBox(height: AboutUsConfig.spacerHeight),
+            ListView.separated(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: languages.length,
+              itemBuilder: (context, index) {
+                final (code, name) = languages[index];
+                final selected = selectedLanguage == code;
+                return DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(NavigationTabViewConfig.radius),
+                    color: context.colorTheme.greyLight,
+                  ),
+                  child: ListTile(
+                    title: Text(name),
+                    trailing: selected ? Icon(Icons.check, color: context.colorTheme.orangePomegranade) : null,
+                    onTap: () {
+                      Navigator.pop(context, code);
+                    },
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(height: AboutUsConfig.spacerHeight),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
