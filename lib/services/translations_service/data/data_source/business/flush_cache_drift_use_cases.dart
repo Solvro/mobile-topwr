ce96bee6a9edc1ac679d698fbe355eca4c48657b
Szymon Kowaliński <szymon@kowalinski.dev>
2025-04-03 14:51:51 +0200

    chore: update accessibility metadata in arb files and improve translation handling

--- lib/services/translations_service/data/data_source/business/flush_cache_drift_use_cases.dart
+++ lib/services/translations_service/data/data_source/business/flush_cache_drift_use_cases.dart
@@ -3,6 +3,7 @@ import "package:flutter_riverpod/flutter_riverpod.dart";
 import "../../../config/translations_config.dart";
 import "../local/database/translation_db_singleton.dart";
 
+// TODO(simon-the-shark): @tomasz-trela move this
 extension FlushCacheDriftUseCases on WidgetRef {
   Future<void> flushOldData() async {
     final db = watch(translationsDatabaseSingletonProvider);
