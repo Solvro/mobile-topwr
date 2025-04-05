edbb3a0adad403749b4a9a2b0777551073b21898
Mikołaj Jałocha <76820915+mikolaj-jalocha@users.noreply.github.com>
2025-04-05 20:19:43 +0300

    feat(guide): migrate to new rest api (#673)
    * feat(guide): add REST repository for post's list
    
    * feat: remove old repository
    
    * feat: implement REST repository
    
    * feat: integrate changes
    
    * feat: replace repository class with function
    
    * feat: refactor package structure
    
    * feat: apply PR suggestions
    
    * feat: add support for images
    
    * ci: add missing example env

---
+++ lib/features/guide_detail_view/data/repository/guide_details_repository.dart
@@ -0,0 +1,30 @@
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:riverpod_annotation/riverpod_annotation.dart";
+
+import "../../../../api_base_rest/cache/cache.dart";
+import "../../../../api_base_rest/client/json.dart";
+import "../../../../config/env.dart";
+import "../../../../config/ttl_config.dart";
+import "../../../guide_view/guide_view.dart";
+import "../models/guide_details.dart";
+
+part "guide_details_repository.g.dart";
+
+@riverpod
+Future<GuideDetails> guideDetailsRepository(Ref ref, int id) async {
+  final apiUrl = Env.mainRestApiUrl;
+  final endpoint = "/guide_articles/$id?description=true&guideQuestions=true&image=true";
+
+  final response =
+      await ref
+          .getAndCacheData(
+            apiUrl + endpoint,
+            TtlStrategy.get(TtlKey.guideDetailsRepository).inDays,
+            GuideDetailsResponse.fromJson,
+            extraValidityCheck: (_) => true,
+            localizedOfflineMessage: GuideView.localizedOfflineMessage,
+            onRetry: ref.invalidateSelf,
+          )
+          .castAsObject;
+  return response.data;
+}
