bb54da33b7cd982cde590d74414fff387ec5dbde
Szymon Kowaliński <szymon@kowalinski.dev>
2025-04-05 19:03:32 +0200

    fix: disable segmented choice controller on the parking view

--- lib/features/bottom_scroll_sheet/sheet_layout_scheme.dart
+++ lib/features/bottom_scroll_sheet/sheet_layout_scheme.dart
@@ -9,6 +9,7 @@ import "../../widgets/search_box_app_bar.dart";
 import "../map_view/controllers/bottom_sheet_controller.dart";
 import "../map_view/controllers/controllers_set.dart";
 import "../map_view/widgets/map_config.dart";
+import "../navigator/navigation_stack.dart";
 import "data_list.dart";
 import "drag_handle.dart";
 import "navigate_button.dart";
@@ -22,6 +23,8 @@ class SheetLayoutScheme<T extends GoogleNavigable> extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final selectedCategory = useState<String>(context.localize.buildings_title);
 
+    final isBuildingMap = ref.watch(currentRouteProvider)?.settings.name == "BuildingsRoute";
+
     final appBar = SearchBoxAppBar(
       context,
       title: context.mapViewTexts<T>().title,
@@ -30,24 +33,29 @@ class SheetLayoutScheme<T extends GoogleNavigable> extends HookConsumerWidget {
       actions: [if (ref.watch(context.activeMarkerController<T>()) != null) NavigateButton<T>()],
     );
 
-    final categoryData = (
-      buildings: (title: context.localize.buildings_title, builder: DataSliverList<T>.new),
-      library: (
-        title: context.localize.library_title,
-        builder: () => const SliverToBoxAdapter(child: Center(child: Text("Lorem ipsum"))),
-      ),
-      showers: (
-        title: context.localize.showers_title,
-        builder: () => const SliverToBoxAdapter(child: Center(child: Text("Lorem ipsum"))),
-      ),
-    );
+    final categoryData =
+        isBuildingMap
+            ? (
+              buildings: (title: context.localize.buildings_title, builder: DataSliverList<T>.new),
+              library: (
+                title: context.localize.library_title,
+                builder: () => const SliverToBoxAdapter(child: Center(child: Text("Lorem ipsum"))),
+              ),
+              showers: (
+                title: context.localize.showers_title,
+                builder: () => const SliverToBoxAdapter(child: Center(child: Text("Lorem ipsum"))),
+              ),
+            )
+            : null;
 
     final validSelectedCategory =
-        categoryData.buildings.title == selectedCategory.value
-            ? categoryData.buildings
-            : categoryData.library.title == selectedCategory.value
-            ? categoryData.library
-            : categoryData.showers;
+        isBuildingMap
+            ? (categoryData!.buildings.title == selectedCategory.value
+                ? categoryData.buildings
+                : categoryData.library.title == selectedCategory.value
+                ? categoryData.library
+                : categoryData.showers)
+            : null;
 
     return CustomScrollView(
       controller: scrollController,
@@ -60,37 +68,38 @@ class SheetLayoutScheme<T extends GoogleNavigable> extends HookConsumerWidget {
           flexibleSpace: appBar,
           automaticallyImplyLeading: false,
         ),
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: SearchBoxAppBar.defaultBottomPadding),
-            child: CupertinoSlidingSegmentedControl<String>(
-              backgroundColor: context.colorTheme.greyLight,
-              thumbColor: context.colorTheme.orangePomegranadeLighter,
-              groupValue: selectedCategory.value,
-              onValueChanged: (value) {
-                if (value != null) {
-                  selectedCategory.value = value;
-                }
-              },
-              children: {
-                categoryData.buildings.title: _SegmentTab(
-                  title: categoryData.buildings.title,
-                  isSelected: selectedCategory.value == categoryData.buildings.title,
-                ),
-                categoryData.library.title: _SegmentTab(
-                  title: categoryData.library.title,
-                  isSelected: selectedCategory.value == categoryData.library.title,
-                ),
-                categoryData.showers.title: _SegmentTab(
-                  title: categoryData.showers.title,
-                  isSelected: selectedCategory.value == categoryData.showers.title,
-                ),
-              },
+        if (categoryData != null)
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: SearchBoxAppBar.defaultBottomPadding),
+              child: CupertinoSlidingSegmentedControl<String>(
+                backgroundColor: context.colorTheme.greyLight,
+                thumbColor: context.colorTheme.orangePomegranadeLighter,
+                groupValue: selectedCategory.value,
+                onValueChanged: (value) {
+                  if (value != null) {
+                    selectedCategory.value = value;
+                  }
+                },
+                children: {
+                  categoryData.buildings.title: _SegmentTab(
+                    title: categoryData.buildings.title,
+                    isSelected: selectedCategory.value == categoryData.buildings.title,
+                  ),
+                  categoryData.library.title: _SegmentTab(
+                    title: categoryData.library.title,
+                    isSelected: selectedCategory.value == categoryData.library.title,
+                  ),
+                  categoryData.showers.title: _SegmentTab(
+                    title: categoryData.showers.title,
+                    isSelected: selectedCategory.value == categoryData.showers.title,
+                  ),
+                },
+              ),
             ),
           ),
-        ),
         const SliverToBoxAdapter(child: SizedBox(height: SearchBoxAppBar.defaultBottomPadding)),
-        validSelectedCategory.builder(),
+        if (validSelectedCategory != null) validSelectedCategory.builder() else DataSliverList<T>(),
       ],
     );
   }
