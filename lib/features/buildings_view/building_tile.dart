c7a562595123302231983bf47dd98f5d09f1c2a0
Szymon Kowaliński <szymon@kowalinski.dev>
2025-04-08 00:48:29 +0200

    feat(buildings): add digital guide navigation button

--- lib/features/buildings_view/building_tile.dart
+++ lib/features/buildings_view/building_tile.dart
@@ -20,49 +20,66 @@ class BuildingTile extends ConsumerWidget {
   final bool isActive;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Stack(
+    return Column(
       children: [
-        PhotoTrailingWideTileCard(
-          activeGradient: context.colorTheme.toPwrGradient,
-          directusPhotoUrl: building.cover?.filename_disk,
-          title: "${building.disableBuildingPrefix ? "" : "${context.localize.building_prefix} "}${building.name}",
-          subtitle: context.changeNull(building.addressFormatted),
-          isActive: isActive,
-          onTap: () {
-            unawaited(ref.read(buildingsMapControllerProvider).onMarkerTap(building));
-          },
-        ),
-        if (building.externalDigitalGuideMode != null && building.externalDigitalGuideIdOrURL != null)
-          Positioned(
-            top: 2,
-            right: WideTileCardConfig.imageSize + 2,
-            child: IconButton(
-              iconSize: 22,
-              visualDensity: VisualDensity.compact,
-              color: switch (building.externalDigitalGuideMode) {
-                "digital_guide_building" || "other_digital_guide_place" => context.colorTheme.orangePomegranade,
-                _ => null,
-              },
-              icon: Icon(
-                switch (building.externalDigitalGuideMode) {
-                  "web_url" =>
-                    building.externalDigitalGuideIdOrURL!.startsWith(UrlConfig.topwrUrl) ? Icons.info : Icons.language,
-                  "digital_guide_building" || "other_digital_guide_place" => Icons.accessibility_new_rounded,
-                  _ => Icons.info,
-                },
-                color:
-                    isActive
-                        ? context.colorTheme.whiteSoap
-                        : switch (building.externalDigitalGuideMode) {
-                          "digital_guide_building" ||
-                          "other_digital_guide_place" => context.colorTheme.orangePomegranade,
-                          _ => context.colorTheme.blackMirage,
-                        },
-              ),
-              onPressed: () {
-                unawaited(ref.navigateBuildingDetailAction(building));
+        Stack(
+          children: [
+            PhotoTrailingWideTileCard(
+              activeGradient: context.colorTheme.toPwrGradient,
+              directusPhotoUrl: building.cover?.filename_disk,
+              title: "${building.disableBuildingPrefix ? "" : "${context.localize.building_prefix} "}${building.name}",
+              subtitle: context.changeNull(building.addressFormatted),
+              isActive: isActive,
+              onTap: () {
+                unawaited(ref.read(buildingsMapControllerProvider).onMarkerTap(building));
               },
             ),
+            if (building.externalDigitalGuideMode != null && building.externalDigitalGuideIdOrURL != null)
+              Positioned(
+                top: 2,
+                right: WideTileCardConfig.imageSize + 2,
+                child: IconButton(
+                  iconSize: 22,
+                  visualDensity: VisualDensity.compact,
+                  color: switch (building.externalDigitalGuideMode) {
+                    "digital_guide_building" || "other_digital_guide_place" => context.colorTheme.orangePomegranade,
+                    _ => null,
+                  },
+                  icon: Icon(
+                    switch (building.externalDigitalGuideMode) {
+                      "web_url" =>
+                        building.externalDigitalGuideIdOrURL!.startsWith(UrlConfig.topwrUrl)
+                            ? Icons.info
+                            : Icons.language,
+                      "digital_guide_building" || "other_digital_guide_place" => Icons.accessibility_new_rounded,
+                      _ => Icons.info,
+                    },
+                    color:
+                        isActive
+                            ? context.colorTheme.whiteSoap
+                            : switch (building.externalDigitalGuideMode) {
+                              "digital_guide_building" ||
+                              "other_digital_guide_place" => context.colorTheme.orangePomegranade,
+                              _ => context.colorTheme.greyPigeon,
+                            },
+                  ),
+                  onPressed: () {
+                    unawaited(ref.navigateBuildingDetailAction(building));
+                  },
+                ),
+              ),
+          ],
+        ),
+        if (isActive &&
+            (building.externalDigitalGuideMode == "digital_guide_building" ||
+                building.externalDigitalGuideMode == "other_digital_guide_place"))
+          TextButton.icon(
+            icon: Icon(Icons.accessibility_new_rounded, color: context.colorTheme.orangePomegranade, size: 16),
+            onPressed: () {
+              unawaited(ref.navigateBuildingDetailAction(building));
+            },
+            style: TextButton.styleFrom(padding: const EdgeInsets.all(12)),
+            label: Text(context.localize.navigate_to_digital_guide, style: context.textTheme.boldBodyOrange),
           ),
       ],
     );
