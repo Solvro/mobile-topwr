3ef05e5ae26303eb88f48060b8220345b75d1be5
macbook <piotr.bonar@gmail.com>
2025-04-16 15:55:02 +0200

    chore(home-screen-banner): final rearranging the changes into better structure

--- lib/widgets/loading_widgets/simple_previews/horizontal_rectangular_section_loading.dart
+++ lib/widgets/loading_widgets/simple_previews/horizontal_rectangular_section_loading.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 
-import "../../../config/ui_config.dart";
 import "../shimmer_loading.dart";
 
 class HorizontalRectangularSectionLoading extends StatelessWidget {
@@ -8,20 +7,11 @@ class HorizontalRectangularSectionLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: HomeViewConfig.paddingMedium),
-      child: ShimmeringEffect(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: HomeViewConfig.paddingMedium),
-          child: Container(
-            width: double.maxFinite,
-            height: 69,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(AppWidgetsConfig.borderRadiusMedium),
-            ),
-          ),
-        ),
+    return ShimmeringEffect(
+      child: Container(
+        width: double.maxFinite,
+        height: 69,
+        decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(8)),
       ),
     );
   }
