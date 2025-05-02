13d9f39004de986209f2bbf99a55d669dcd6f4df
Szymon Kowaliński <szymon@kowalinski.dev>
2025-05-02 14:21:43 +0200

    feat: some other padding-wise changes

--- lib/features/science_club/science_clubs_view/widgets/science_clubs_info_dialog.dart
+++ lib/features/science_club/science_clubs_view/widgets/science_clubs_info_dialog.dart
@@ -7,55 +7,74 @@ import "../../../../config/ui_config.dart";
 import "../../../../theme/app_theme.dart";
 import "../../../../utils/context_extensions.dart";
 import "../../../../utils/launch_url_util.dart";
-import "../../../../widgets/my_text_button.dart";
 
 class ScienceClubInfoDialog extends StatelessWidget {
   const ScienceClubInfoDialog({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(ScienceClubsViewConfig.rectangleBorderRadius)),
-      child: Padding(
-        padding: const EdgeInsets.all(
-          ScienceClubsViewConfig.mediumPadding,
-        ).copyWith(bottom: ScienceClubsViewConfig.smallPadding),
-        child: Column(
-          spacing: ScienceClubsViewConfig.listSeparatorSize,
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              context.localize.add_club_contact_info_question,
-              textAlign: TextAlign.center,
-              style: context.textTheme.headline,
-            ),
-
-            Consumer(
-              builder:
-                  (context, ref, child) => RichText(
-                    text: TextSpan(
-                      text: context.localize.add_club_contact_info,
-                      style: context.textTheme.lightTitle,
-                      children: [
-                        TextSpan(
-                          text: context.localize.report_change_email,
-                          style: context.textTheme.lightTitleOrange.copyWith(decoration: TextDecoration.underline),
-                          recognizer:
-                              TapGestureRecognizer()
-                                ..onTap = () async {
-                                  final emailUrl = "mailto:${context.localize.report_change_email}";
-                                  unawaited(ref.launch(emailUrl));
-                                },
-                        ),
-                        const TextSpan(text: "."),
-                      ],
-                    ),
-                  ),
-            ),
-            MyTextButton(onClick: () => Navigator.of(context).pop(), actionTitle: context.localize.ok),
-          ],
+    const horizontalPadding = 36.0;
+    const bottomPadding = 24.0;
+    return AlertDialog(
+      titlePadding: const EdgeInsets.only(left: horizontalPadding, top: 32, right: horizontalPadding),
+      contentPadding: const EdgeInsets.only(
+        left: horizontalPadding,
+        top: 16,
+        right: horizontalPadding,
+        bottom: bottomPadding / 1.5,
+      ),
+      actionsPadding: const EdgeInsets.only(
+        bottom: bottomPadding,
+        left: horizontalPadding / 1.5,
+        right: horizontalPadding / 1.5,
+      ),
+      actionsAlignment: MainAxisAlignment.center,
+      title: Text(context.localize.add_club_contact_info_question, style: context.textTheme.headline),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          style: TextButton.styleFrom(padding: const EdgeInsets.all(12)),
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  context.localize.ok,
+                  textAlign: TextAlign.center,
+                  style: context.textTheme.boldBodyOrange.copyWith(color: context.colorTheme.orangePomegranade),
+                ),
+              ),
+            ],
+          ),
         ),
+      ],
+      content: Column(
+        spacing: ScienceClubsViewConfig.listSeparatorSize,
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Consumer(
+            builder:
+                (context, ref, child) => RichText(
+                  text: TextSpan(
+                    text: context.localize.add_club_contact_info,
+                    style: context.textTheme.lightTitle,
+                    children: [
+                      TextSpan(
+                        text: context.localize.report_change_email,
+                        style: context.textTheme.lightTitleOrange.copyWith(decoration: TextDecoration.underline),
+                        recognizer:
+                            TapGestureRecognizer()
+                              ..onTap = () async {
+                                final emailUrl = "mailto:${context.localize.report_change_email}";
+                                unawaited(ref.launch(emailUrl));
+                              },
+                      ),
+                      const TextSpan(text: "."),
+                    ],
+                  ),
+                ),
+          ),
+        ],
       ),
     );
   }
