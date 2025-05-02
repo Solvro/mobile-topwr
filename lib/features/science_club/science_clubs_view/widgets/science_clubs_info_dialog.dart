276385762f6f621ce9d8eb2bbedfcb9cbfe48907
Szymon Kowaliński <szymon@kowalinski.dev>
2025-05-02 12:41:15 +0200

    feat: add clicable email

--- lib/features/science_club/science_clubs_view/widgets/science_clubs_info_dialog.dart
+++ lib/features/science_club/science_clubs_view/widgets/science_clubs_info_dialog.dart
@@ -1,6 +1,13 @@
+import "dart:async";
+
+import "package:flutter/gestures.dart" show TapGestureRecognizer;
 import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 import "../../../../config/ui_config.dart";
 import "../../../../l10n/app_localizations.dart";
+import "../../../../theme/app_theme.dart";
+import "../../../../utils/context_extensions.dart";
+import "../../../../utils/launch_url_util.dart";
 
 class ScienceClubInfoDialog extends StatelessWidget {
   const ScienceClubInfoDialog({super.key});
@@ -15,12 +22,33 @@ class ScienceClubInfoDialog extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              AppLocalizations.of(context)!.add_club_contact_info_question,
+              context.localize.add_club_contact_info_question,
               textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headlineSmall,
+              style: context.textTheme.body,
             ),
             const SizedBox(height: ScienceClubsViewConfig.listSeparatorSize),
-            Text(AppLocalizations.of(context)!.add_club_contact_info, textAlign: TextAlign.center),
+            Consumer(
+              builder:
+                  (context, ref, child) => RichText(
+                    text: TextSpan(
+                      text: context.localize.add_club_contact_info,
+                      style: context.textTheme.body,
+                      children: [
+                        TextSpan(
+                          text: context.localize.report_change_email,
+                          style: context.textTheme.bodyOrange.copyWith(decoration: TextDecoration.underline),
+                          recognizer:
+                              TapGestureRecognizer()
+                                ..onTap = () async {
+                                  final emailUrl = "mailto:${context.localize.report_change_email}";
+                                  unawaited(ref.launch(emailUrl));
+                                },
+                        ),
+                        const TextSpan(text: "."),
+                      ],
+                    ),
+                  ),
+            ),
             const SizedBox(height: ScienceClubsViewConfig.listSeparatorSize),
             TextButton(onPressed: () => Navigator.of(context).pop(), child: Text(AppLocalizations.of(context)!.ok)),
           ],
