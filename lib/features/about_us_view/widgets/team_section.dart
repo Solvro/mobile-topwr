24f82c1d1f6677ef3a64c20bf38b74ae33b711d1
mikolaj-jalocha <mikics2k17@gmail.com>
2025-05-04 16:19:19 +0200

    feat: add support for large fonts

--- lib/features/about_us_view/widgets/team_section.dart
+++ lib/features/about_us_view/widgets/team_section.dart
@@ -180,17 +180,16 @@ class _TeamMemberCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: WideTileCardConfig.imageSize,
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 8),
-        child: Container(
-          width: double.infinity,
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(
-            color: context.colorTheme.greyLight,
-            borderRadius: BorderRadius.circular(AboutUsConfig.borderRadius),
-          ),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Container(
+        width: double.infinity,
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          color: context.colorTheme.greyLight,
+          borderRadius: BorderRadius.circular(AboutUsConfig.borderRadius),
+        ),
+        child: IntrinsicHeight(
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
@@ -201,7 +200,9 @@ class _TeamMemberCard extends StatelessWidget {
                 ),
               ),
               const SizedBox(width: 14),
-              _Description(name: member.name ?? "", subtitle: member.subtitle ?? "", links: member.links),
+              Flexible(
+                child: _Description(name: member.name ?? "", subtitle: member.subtitle ?? "", links: member.links),
+              ),
             ],
           ),
         ),
@@ -239,24 +240,28 @@ class _Description extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(name, style: context.aboutUsTheme.headlineSmaller),
-        const SizedBox(height: 4),
-        Text(subtitle, style: context.aboutUsTheme.bodySmaller),
-        const SizedBox(height: 8),
-        Row(
+    return Expanded(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            for (final icon in links)
-              Semantics(
-                label: "${context.localize.button_leading_to}: ${icon.url?.substring(7)}",
-                child: _Icon(launchUrl: icon.url ?? "", icon: icon.icon),
-              ),
+            Text(name, style: context.aboutUsTheme.headlineSmaller, softWrap: true),
+            const SizedBox(height: 4),
+            Text(subtitle, style: context.aboutUsTheme.bodySmaller, softWrap: true),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                for (final icon in links)
+                  Semantics(
+                    label: "${context.localize.button_leading_to}: ${Uri.parse(icon.url ?? "").host}",
+                    child: _Icon(launchUrl: icon.url ?? "", icon: icon.icon),
+                  ),
+              ],
+            ),
           ],
         ),
-      ],
+      ),
     );
   }
 }
