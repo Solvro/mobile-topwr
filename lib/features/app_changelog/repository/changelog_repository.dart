db5bc1ae78e163d2a716ae54806c36c7bc67f7b8
Szymon Kowaliński <szymon@kowalinski.dev>
2025-05-06 21:14:01 +0200

    fix(changelog): translate changes descriptions

--- lib/features/app_changelog/repository/changelog_repository.dart
+++ lib/features/app_changelog/repository/changelog_repository.dart
@@ -3,6 +3,7 @@ import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
 import "../../../api_base/query_adapter.dart";
+import "../../../api_base/translations/temp_graphql_translate.dart";
 import "../../../config/ttl_config.dart";
 import "get_changelog.graphql.dart";
 
@@ -13,6 +14,14 @@ typedef Changelog = Query$GetChangelog$Changelog;
 @riverpod
 Future<IList<Changelog>> changelogRepository(Ref ref) async {
   final results = await ref.queryGraphql(Options$Query$GetChangelog(), TtlKey.changelogRepository);
-
-  return results?.Changelog.toIList() ?? const IList<Changelog>.empty();
+  return ref.translateGraphQLModelIList(
+    results?.Changelog.toIList() ?? const IList<Changelog>.empty(),
+    (changelog) async => changelog.copyWith(
+      changes: await ref.translateGraphQLModelList(
+        changelog.changes ?? [],
+        (change) async =>
+            change?.copyWith(changeDescription: await ref.translateGraphQLString(change.changeDescription)),
+      ),
+    ),
+  );
 }
