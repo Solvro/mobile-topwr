78fa4cc342b0d83adf7774cb91dcf5e93db5989f
Maja Mroczek <152724796+mmzek@users.noreply.github.com>
2025-05-15 22:10:59 +0200

    feat(science-clubs): migrate to rest for student clubs (#722)
    * chore: delete unused files
    
    * feat: create repository and model files
    
    * fix: apply changes to science clubs files
    
    * fix: fix model problems
    
    * fix: add department relation
    
    * feat: add isStrategic field

---
+++ lib/features/science_club/science_clubs_view/model/science_clubs.dart
@@ -0,0 +1,61 @@
+import "package:fast_immutable_collections/fast_immutable_collections.dart";
+import "package:freezed_annotation/freezed_annotation.dart";
+
+part "science_clubs.freezed.dart";
+part "science_clubs.g.dart";
+
+@freezed
+abstract class ScienceClub with _$ScienceClub {
+  const factory ScienceClub({
+    required int id,
+    required String name,
+    required String organizationStatus,
+    required String source,
+    required String organizationType,
+    required bool coverPreview,
+    required bool isStrategic,
+    IList<TagsData>? tags,
+    LogoData? logo,
+    CoverData? cover,
+    String? shortDescription,
+    Department? department,
+  }) = _ScienceClub;
+
+  factory ScienceClub.fromJson(Map<String, dynamic> json) => _$ScienceClubFromJson(json);
+}
+
+@freezed
+abstract class ScienceClubsResponse with _$ScienceClubsResponse {
+  const factory ScienceClubsResponse({required IList<ScienceClub> data}) = _ScienceClubsResponse;
+
+  factory ScienceClubsResponse.fromJson(Map<String, dynamic> json) => _$ScienceClubsResponseFromJson(json);
+}
+
+@freezed
+abstract class Department with _$Department {
+  const factory Department({required int id, required String name, required String code, required String betterCode}) =
+      _Department;
+
+  factory Department.fromJson(Map<String, dynamic> json) => _$DepartmentFromJson(json);
+}
+
+@freezed
+abstract class TagsData with _$TagsData {
+  const factory TagsData({required String tag}) = _TagsData;
+
+  factory TagsData.fromJson(Map<String, dynamic> json) => _$TagsDataFromJson(json);
+}
+
+@freezed
+abstract class LogoData with _$LogoData {
+  const factory LogoData({required String id, required String url}) = _LogoData;
+
+  factory LogoData.fromJson(Map<String, dynamic> json) => _$LogoDataFromJson(json);
+}
+
+@freezed
+abstract class CoverData with _$CoverData {
+  const factory CoverData({required String id, required String url}) = _CoverData;
+
+  factory CoverData.fromJson(Map<String, dynamic> json) => _$CoverDataFromJson(json);
+}
