5dab6fc20ed8c712fd8fd926e2dd577179a11b00
mikolaj-jalocha <mikics2k17@gmail.com>
2025-05-17 19:38:03 +0200

    feat: setup data layer

--- lib/features/buildings_view/model/building_model.dart
+++ lib/features/buildings_view/model/building_model.dart
@@ -2,27 +2,33 @@ import "package:latlong2/latlong.dart";
 
 import "../../../config/map_view_config.dart";
 import "../../map_view/controllers/controllers_set.dart";
-import "../repository/buildings_repository.dart";
+import "building_data.dart";
 
-class BuildingModel extends Building implements GoogleNavigable {
-  BuildingModel.from(Building building)
-    : super(
-        id: building.id,
-        name: building.name,
-        longitude: building.longitude,
-        latitude: building.latitude,
-        addres: building.addres,
-        cover: building.cover,
-        naturalName: building.naturalName,
-        disableBuildingPrefix: building.disableBuildingPrefix,
-        externalDigitalGuideMode: building.externalDigitalGuideMode,
-        externalDigitalGuideIdOrURL: building.externalDigitalGuideIdOrURL,
-      );
+class BuildingModel implements GoogleNavigable {
+  @override
+  final String id;
+  final String name;
+  final String naturalName;
+  final String address;
+  final double latitude;
+  final double longitude;
+  final String coverUrl;
+  final String? externalDigitalGuideMode;
+  final String? externalDigitalGuideIdOrURL;
+
+  BuildingModel.from(BuildingData building)
+    : id = building.id.toString(),
+      name = building.identifier,
+      naturalName = building.naturalName ?? "",
+      address = "${building.addressLine1}\n${building.addressLine2}".replaceFirst(",", "\n").replaceAll("\n ", "\n"),
+      latitude = building.latitude,
+      longitude = building.longitude,
+      coverUrl = building.cover?.url ?? "",
+      externalDigitalGuideMode = building.externalDigitalGuideMode,
+      externalDigitalGuideIdOrURL = building.externalDigitalGuideIdOrUrl;
 
   @override
   LatLng get location => LatLng(latitude, longitude);
-
-  String? get addressFormatted => addres?.replaceFirst(",", "\n").replaceAll("\n ", "\n");
 
   String? get parseBuildingCode {
     final List<String> separatedBuildingName = name.split(BuildingSearchConfig.buildingCodeSeperator);
