42c59c43836156cc6f09906f0d6f0710e4c355ca
mikolaj-jalocha <mikics2k17@gmail.com>
2025-06-15 10:13:02 +0200

    feat: add accessibility support for update dialog

--- lib/features/app_changelog/widgets/app_changelog_header.dart
+++ lib/features/app_changelog/widgets/app_changelog_header.dart
@@ -10,28 +10,34 @@ class AppChangelogHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text.rich(
-          TextSpan(
-            children: [
-              TextSpan(text: context.localize.changes, style: context.textTheme.headlineOrange),
-              const WidgetSpan(child: SizedBox(width: 5)),
-              TextSpan(text: "${context.localize.version} ${version.toUpperCase()}", style: context.textTheme.bodyGrey),
-            ],
+    return Semantics(
+      container: true,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(text: context.localize.changes, style: context.textTheme.headlineOrange),
+                const WidgetSpan(child: SizedBox(width: 5)),
+                TextSpan(
+                  text: "${context.localize.version} ${version.toUpperCase()}",
+                  style: context.textTheme.bodyGrey,
+                ),
+              ],
+            ),
           ),
-        ),
-        const SizedBox(height: 8),
-        Text(context.localize.hey_check_out_the_new_features, style: context.textTheme.headline),
-        const SizedBox(height: 5),
-        Text(
-          context.localize.here_you_will_see_what_we_changed_in_the_latest_version,
-          style: context.textTheme.bodyGrey,
-          overflow: TextOverflow.clip,
-        ),
-      ],
+          const SizedBox(height: 8),
+          Text(context.localize.hey_check_out_the_new_features, style: context.textTheme.headline),
+          const SizedBox(height: 5),
+          Text(
+            context.localize.here_you_will_see_what_we_changed_in_the_latest_version,
+            style: context.textTheme.bodyGrey,
+            overflow: TextOverflow.clip,
+          ),
+        ],
+      ),
     );
   }
 }
