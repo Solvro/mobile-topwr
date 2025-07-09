3654be9fe6107971e04cb8a62b5cf3988180c48c
Mikołaj Jałocha <76820915+mikolaj-jalocha@users.noreply.github.com>
2025-07-10 00:17:25 +0200

    feat: migrate changelog to REST API (#775)
    * feat: delete old files
    
    * feat: setup data layer
    
    * feat: setup UI layer
    
    * fix: remove unused fields and add  annotation

--- lib/features/app_changelog/show_changelog.dart
+++ lib/features/app_changelog/show_changelog.dart
@@ -1,41 +1,18 @@
-import "package:collection/collection.dart";
-import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
-import "package:package_info_plus/package_info_plus.dart";
 
 import "app_changelog.dart";
-import "repository/changelog_repository.dart";
-import "repository/local_changelog_repository.dart";
+import "data/repository/local_changelog_repository.dart";
+import "data/repository/my_changelog_repository.dart";
 
 Future<void> showChangelog(BuildContext context, WidgetRef ref) async {
-  final changelogs = await ref.read(changelogRepositoryProvider.future);
-  if (changelogs.isEmpty) return;
+  final changelog = await ref.read(changelogRepositoryProvider.future);
 
-  final appVersion = await _getAppVersion();
-  final changelogForCurrentVersion = _findChangelogForVersion(changelogs, appVersion);
+  if (changelog == null) return;
 
-  if (changelogForCurrentVersion == null) return;
+  final changelogSeen = await ref.read(localChangelogRepositoryProvider(changelog.name).future) ?? true;
 
-  final changelogSeen =
-      await ref.read(localChangelogRepositoryProvider(changelogForCurrentVersion.versionString!).future) ?? true;
-
-  if (!changelogSeen && context.mounted && _canShowDialog(changelogForCurrentVersion)) {
-    await showDialog<void>(context: context, builder: (context) => AppChangelog(changelog: changelogForCurrentVersion));
+  if (!changelogSeen && context.mounted && changelog.changes.isNotEmpty) {
+    await showDialog<void>(context: context, builder: (context) => AppChangelog(changelog: changelog));
   }
-}
-
-bool _canShowDialog(Changelog changelog) {
-  final changes = changelog.changes;
-
-  return changelog.versionString != null && changes != null && changes.every((change) => change != null);
-}
-
-Future<String> _getAppVersion() async {
-  final packageInfo = await PackageInfo.fromPlatform();
-  return packageInfo.version;
-}
-
-Changelog? _findChangelogForVersion(IList<Changelog> changelogs, String version) {
-  return changelogs.firstWhereOrNull((changelog) => changelog.versionString == version);
 }
