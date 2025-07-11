461314cc55d961b419d9ba9dbef6eed9e181d143
Szymon Kowaliński <szymon@kowalinski.dev>
2025-07-11 03:14:38 +0200

    feat(remote-config): add remote REST config

--- lib/api_base/cache/remote_flush/data/remote_cache_ref_number_repo.dart
+++ lib/api_base/cache/remote_flush/data/remote_cache_ref_number_repo.dart
@@ -1,16 +1,12 @@
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
-import "../../../gql_client_provider.dart";
-import "../../../query_adapter.dart";
-import "getRemoteCacheRef.graphql.dart";
+import "../../../../features/remote_config/data/repository/remote_config_repository.dart";
 
 part "remote_cache_ref_number_repo.g.dart";
 
 @riverpod
 Future<int?> remoteCacheRefNumberRepo(Ref ref) async {
-  final apiClient = await ref.watch(grapqlClientProvider.future);
-  final remoteValue = await apiClient.query$GetRemoteRefCacheNumber();
-  final safeRemoteValue = await GqlOfflineException.graphqlTryCatch(remoteValue, null);
-  return safeRemoteValue.parsedData?.CacheReferenceNumber?.referenceNumber;
+  final remoteConfig = await ref.watch(remoteConfigRepositoryProvider.future);
+  return remoteConfig.referenceNumber;
 }
