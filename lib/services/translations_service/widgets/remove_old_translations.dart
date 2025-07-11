76643c80f3c35b77b5b470a5920552fa59da851b
Szymon Kowaliński <szymon@kowalinski.dev>
2025-07-11 02:07:26 +0200

    fix: add unwaited microtask util (#778)

--- lib/services/translations_service/widgets/remove_old_translations.dart
+++ lib/services/translations_service/widgets/remove_old_translations.dart
@@ -1,9 +1,8 @@
-import "dart:async";
-
 import "package:flutter/widgets.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 
 import "../../../hooks/use_effect_on_init.dart";
+import "../../../utils/unwaited_microtask.dart";
 import "../business/flush_cache_drift_use_cases.dart";
 
 class RemoveOldTranslations extends HookConsumerWidget {
@@ -14,7 +13,7 @@ class RemoveOldTranslations extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     useEffectOnInit(() {
-      unawaited(Future.microtask(ref.flushOldData));
+      unwaitedMicrotask(ref.flushOldData);
       return null;
     });
     return child;
