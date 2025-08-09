38342e96770c80dc4f3c3ddb95496da108dfbb11
mikolaj-jalocha <mikics2k17@gmail.com>
2025-08-09 12:02:10 +0200

    refactor: introduce enum as wm task name, extract DIO client config for parking, add support for different works in task handler

--- lib/work_manager/background_task_handler.dart
+++ lib/work_manager/background_task_handler.dart
@@ -3,27 +3,34 @@ import "dart:async";
 import "package:dio/dio.dart";
 import "package:workmanager/workmanager.dart";
 
-import "../config/env.dart";
 import "../features/home_screen_app_widgets/widget_service.dart";
 import "../features/parkings/parkings_view/api_client/iparking_client.dart";
-import "../features/parkings/parkings_view/models/parking.dart";
+import "../features/parkings/parkings_view/repository/parkings_repository.dart";
 import "../utils/ilist_nonempty.dart";
+import "parking_widget_work_manger.dart";
 
+// TODO(mikolaj-jalocha): check if WorkManager can be replaced by AlarmManager
 @pragma("vm:entry-point")
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
-    final parkings = await _fetchParkings();
-    await WidgetService.updateParkingSlots(parkings.toIList());
-
-    return Future.value(true);
+    final Dio parkingClient = getIParkingClient();
+    final taskEnum = ParkingWorkManagerTask.fromUniqueTaskName(task);
+    switch (taskEnum) {
+      case ParkingWorkManagerTask.immediate:
+        while (true) {
+          await _fetchDataAndUpdateParkingWidget(parkingClient);
+          await Future<void>.delayed(const Duration(seconds: 60));
+        }
+      case ParkingWorkManagerTask.periodic:
+        await _fetchDataAndUpdateParkingWidget(parkingClient);
+        return Future.value(true);
+      case null:
+        return Future.value(true);
+    }
   });
 }
 
-// TODO(mikolaj-jalocha): find better way for doing this
-Future<List<Parking>> _fetchParkings() async {
-  final Dio dio = Dio(BaseOptions(baseUrl: ParkingsConfig.soapFullUrl, headers: ParkingsConfig.headers));
-  final url = Env.parkingApiUrl;
-  final response = await dio.get<Map<String, dynamic>>(url);
-  final places = response.data?["places"] as List<dynamic>;
-  return places.whereType<Map<String, dynamic>>().map(Parking.fromJson).toList();
+Future<void> _fetchDataAndUpdateParkingWidget(Dio parkingClient) async {
+  final parkings = await parkingClient.fetchParkings();
+  await WidgetService.updateParkingSlots(parkings.toIList());
 }
--- lib/work_manager/parking_widget_work_manger.dart
+++ lib/work_manager/parking_widget_work_manger.dart
@@ -2,8 +2,33 @@ import "package:workmanager/workmanager.dart";
 
 import "background_task_handler.dart";
 
+enum ParkingWorkManagerTask {
+  immediate("immediate"),
+  periodic("periodic");
+
+  final String taskName;
+
+  static ParkingWorkManagerTask? fromUniqueTaskName(String name) {
+    for (final task in ParkingWorkManagerTask.values) {
+      if (task.taskName == name) return task;
+    }
+    return null;
+  }
+
+  const ParkingWorkManagerTask(this.taskName);
+}
+
 Future<void> setupParkingWidgetsWorkManager() async {
   await Workmanager().initialize(callbackDispatcher);
-  await Workmanager().registerOneOffTask("immediate_task", "widget_wm");
-  await Workmanager().registerPeriodicTask("periodic_task", "widget_wm", frequency: const Duration(minutes: 1));
+  await Workmanager().registerOneOffTask(
+    ParkingWorkManagerTask.immediate.taskName,
+    ParkingWorkManagerTask.immediate.taskName,
+    existingWorkPolicy: ExistingWorkPolicy.keep,
+  );
+  await Workmanager().registerPeriodicTask(
+    ParkingWorkManagerTask.periodic.taskName,
+    ParkingWorkManagerTask.periodic.taskName,
+    frequency: const Duration(minutes: 1),
+    existingWorkPolicy: ExistingWorkPolicy.keep,
+  );
 }
