7602a22b8a3b51b13e652eb6827dfd7b1805f084
Szymon Kowaliński <szymon@kowalinski.dev>
2025-08-31 17:19:05 +0200

    chore(multilayer-map): update BuildingNavLink widget with new icon and navigation logic

--- lib/features/multilayer_map/presentation/widgets/building_nav_link.dart
+++ lib/features/multilayer_map/presentation/widgets/building_nav_link.dart
@@ -6,8 +6,9 @@ import "package:hooks_riverpod/hooks_riverpod.dart";
 import "../../../../config/ui_config.dart";
 import "../../../../theme/app_theme.dart";
 import "../../../../utils/context_extensions.dart";
-import "../../../navigator/utils/navigation_commands.dart";
 import "../../data/model/building.dart";
+import "../../data/model/multilayer_item.dart";
+import "../controllers.dart";
 
 class BuildingNavLink extends ConsumerWidget {
   const BuildingNavLink({super.key, required this.building, required this.isActive});
@@ -22,13 +23,10 @@ class BuildingNavLink extends ConsumerWidget {
         tooltip: "${context.localize.building_nav_link_tooltip} ${building.name}",
         iconSize: context.textScaler.clamp(maxScaleFactor: 2).scale(22),
         visualDensity: VisualDensity.compact,
-        color: isActive ? context.colorTheme.whiteSoap : context.colorTheme.greyPigeon,
-        icon: Icon(
-          Icons.account_balance_outlined,
-          color: isActive ? context.colorTheme.whiteSoap : context.colorTheme.greyPigeon,
-        ),
+        color: isActive ? context.colorTheme.whiteSoap : context.colorTheme.blueAzure,
+        icon: Icon(Icons.home_sharp, color: isActive ? context.colorTheme.whiteSoap : context.colorTheme.blueAzure),
         onPressed: () {
-          unawaited(ref.navigateBuildingDetailAction(building));
+          unawaited(ref.read(multilayerMapControllerProvider).onMarkerTap(BuildingItem(building: building)));
         },
       ),
     );
