c23a2e392ebbd814a49ee8b47c4d48a70a3b0eb3
tomasz-trela <trela.tomasz@protonmail.com>
2025-09-30 12:03:17 +0200

    feat: branches in departments

--- lib/features/departments/departments_view/data/repository/departments_repository.dart
+++ lib/features/departments/departments_view/data/repository/departments_repository.dart
@@ -5,6 +5,8 @@ import "package:riverpod_annotation/riverpod_annotation.dart";
 import "../../../../../api_base_rest/client/json.dart";
 import "../../../../../api_base_rest/translations/translate.dart";
 import "../../../../../config/env.dart";
+import "../../../../branches/buisiness/branch_provider.dart";
+import "../../../../branches/model/branch.dart";
 import "../../departments_view.dart";
 import "../models/department.dart";
 
@@ -24,5 +26,14 @@ Future<IList<Department>> departmentsRepository(Ref ref) async {
       )
       .castAsObject;
 
-  return response.data;
+  return response.data.sortByBranch(await ref.watch(branchRepositoryProvider.future)).toIList();
+}
+
+extension SortBySourceTypeX on Iterable<Department> {
+  List<Department> sortByBranch(Branch? branch) {
+    if (branch == null) return toList();
+    final branchDepartments = where((element) => element.branch == branch);
+    final otherDepartments = where((element) => element.branch != branch);
+    return [...branchDepartments, ...otherDepartments];
+  }
 }
