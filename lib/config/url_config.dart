d4c467ed6609cd4a6f5edecdd5b6d9ea981f8c3b
Szymon Kowaliński <szymon@kowalinski.dev>
2025-10-09 09:24:59 +0200

    feat(news): add read more button

--- lib/config/url_config.dart
+++ lib/config/url_config.dart
@@ -2,4 +2,7 @@ abstract class UrlConfig {
   static const academicCalendarUrl = "https://pwr.edu.pl/studenci/kalendarz-akademicki";
 
   static const topwrUrl = "https://topwr.solvro.pl/";
+
+  static const newsfeedUrlPL = "https://pwr.edu.pl/uczelnia/aktualnosci";
+  static const newsfeedUrlENG = "https://pwr.edu.pl/en/university/news";
 }
--- lib/features/newsfeed/presentation/news_list_view.dart
+++ lib/features/newsfeed/presentation/news_list_view.dart
@@ -2,12 +2,16 @@ import "package:auto_route/auto_route.dart";
 import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:solvro_translator_core/solvro_translator_core.dart";
 
 import "../../../../widgets/my_error_widget.dart";
 import "../../../config/ui_config.dart";
+import "../../../config/url_config.dart";
+import "../../../services/translations_service/data/preferred_lang_repository.dart";
 import "../../../utils/context_extensions.dart";
 import "../../../utils/launch_url_util.dart";
 import "../../../widgets/detail_views/detail_view_app_bar.dart";
+import "../../../widgets/my_text_button.dart";
 import "../../../widgets/wide_tile_card.dart";
 import "../../departments/departments_view/widgets/departments_view_loading.dart";
 import "../../guide_view/widgets/guide_grid.dart";
@@ -40,7 +44,25 @@ class _NewsfeedViewContent extends ConsumerWidget {
 
     return switch (newsfeed) {
       AsyncError(:final error, :final stackTrace) => MyErrorWidget(error, stackTrace: stackTrace),
-      AsyncValue(:final IList<Article> value) => GuideGrid(children: [for (final item in value) NewsTile(item)].lock),
+      AsyncValue(:final IList<Article> value) => GuideGrid(
+        children: [
+          for (final item in value) NewsTile(item),
+          Align(
+            alignment: Alignment.topCenter,
+            child: SizedBox(
+              width: double.infinity,
+              child: MyTextButton(
+                actionTitle: context.localize.read_more_arrows,
+                onClick: () => ref.launch(
+                  ref.watch(preferredLanguageRepositoryProvider).valueOrNull != SolvroLocale.pl
+                      ? UrlConfig.newsfeedUrlENG
+                      : UrlConfig.newsfeedUrlPL,
+                ),
+              ),
+            ),
+          ),
+        ].lock,
+      ),
       _ => const Padding(padding: GuideViewConfig.gridPadding, child: DepartmentsViewLoading()),
     };
   }
