301f9bc512a4721603f4742a741435fd91025c3f
Bartosh <101900992+24bartixx@users.noreply.github.com>
2025-10-15 23:43:54 +0200

    feat(radio-luz): add radio player  (#861)
    * feat: add radio player
    
    * fix: change icon
    
    * feat: play in background
    
    * fix: just audio background set up on android
    
    * fix: waiting state of radio action button
    
    * chore: review suggestions
    
    * refactor: use riverpod
    
    * feat: add radio luz image
    
    * refactor: use riverpod for audio player streams
    
    * fix: add initial stoper state
    
    * fix: try to fix radio luz api tests
    
    * fix: Revert "fix: try to fix radio luz api tests"
    
    This reverts commit d699728de24f2bc03dd92e5b2f205e74062be965.
    
    * fix(radio-luz): add ThumbnailConverter for nullable thumbnail handling in Schedule model
    
    * feat: load the radio after user enter radio screen
    
    * chore: clean up
    
    ---------
    
    Co-authored-by: Szymon Kowaliński <szymon@kowalinski.dev>

---
+++ lib/features/luz_radio/presentation/radio_luz_view.dart
@@ -0,0 +1,18 @@
+import "package:auto_route/auto_route.dart";
+import "package:flutter/material.dart";
+
+import "../../../widgets/detail_views/detail_view_app_bar.dart";
+import "audio_player_widget.dart";
+
+@RoutePage()
+class RadioLuzView extends StatelessWidget {
+  const RadioLuzView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: DetailViewAppBar(),
+      body: const Center(child: AudioPlayerWidget()),
+    );
+  }
+}
