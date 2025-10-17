f015d4b8db09328418b8a0bf84936a881b066880
Anna Sidor <annasidor00@gmail.com>
2025-10-18 00:29:44 +0200

    feat: everything except stateful lists

--- lib/features/sks/sks_favourite_dishes/utils/sks_favourite_dishes_extension.dart
+++ lib/features/sks/sks_favourite_dishes/utils/sks_favourite_dishes_extension.dart
@@ -1,11 +1,18 @@
 import "package:dio/dio.dart";
 
 import "../../../../config/env.dart";
+import "../../../../utils/get_device_id.dart";
 
 extension DioFetchSksFavouriteDishes on Dio {
   Future<void> toggleSubscription(String dishId, {required bool subscribe}) async {
     final url = "${Env.sksUrl}/subscriptions/toggle";
-    const key = "aas";
+    final key = await getDeviceId();
+    if (key == null) {
+      throw DioException(
+        requestOptions: RequestOptions(path: url),
+        message: "Device ID is null",
+      );
+    }
     final dishIdNum = int.tryParse(dishId);
     await post<Map<String, dynamic>>(
       url,
