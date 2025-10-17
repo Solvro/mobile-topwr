e0284fa65a0b8bcf127ceb0ff707cd6330834680
Anna Sidor <annasidor00@gmail.com>
2025-10-17 16:28:24 +0200

    feat: mock device register to backend

---
+++ lib/register_for_notifications.dart
@@ -0,0 +1,11 @@
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+import "api_base_rest/client/dio_client.dart";
+import "config/env.dart";
+
+Future<void> registerForNotifications(Ref ref) async {
+  final client = ref.read(restClientProvider);
+  final url = "${Env.sksUrl}/device/registration-token";
+  final response = await client.put<Map<String, dynamic>>(url, data: {"deviceKey": "aa", "registrationToken": "bb"});
+  print("FCM registration response: ${response.data}");
+}
