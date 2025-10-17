44caec608488fec238f755f5e3a4274ac1e520db
Anna Sidor <annasidor00@gmail.com>
2025-10-17 16:28:24 +0200

    feat: mock device register to backend

--- lib/features/splash_screen/splash_screen_controller.dart
+++ lib/features/splash_screen/splash_screen_controller.dart
@@ -7,6 +7,7 @@ import "package:riverpod_annotation/riverpod_annotation.dart";
 
 import "../../config/ui_config.dart";
 import "../../firebase_init.dart";
+import "../../register_for_notifications.dart";
 import "../home_view/widgets/logo_app_bar.dart";
 import "../map_view/data/cache.dart";
 
@@ -22,6 +23,7 @@ class SplashScreenController extends _$SplashScreenController {
     WidgetsFlutterBinding.ensureInitialized();
     await firebaseInit();
     await AppBarLogo.precacheImageIfAbsent();
+    await registerForNotifications(ref);
     await ref.read(mapCacheStoreProvider.future); // prefetch map cache directory
   }
 
