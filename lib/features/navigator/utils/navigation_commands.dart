7f2ee8f97227bd9d5888b3f7d287f5549fe7d77e
Anna Sidor <annasidor00@gmail.com>
2025-10-21 23:31:15 +0200

    refactor: all simple views with filter deeplink support

--- lib/features/navigator/utils/navigation_commands.dart
+++ lib/features/navigator/utils/navigation_commands.dart
@@ -62,7 +62,7 @@ extension NavigationX on WidgetRef {
 
   Future<void> navigateDepartments() async {
     await trackEvent(UmamiEvents.openDepartmentsList);
-    await _router.push(const DepartmentsRoute());
+    await _router.push(DepartmentsRoute());
   }
 
   Future<void> navigateDepartmentDetail(int id) async {
