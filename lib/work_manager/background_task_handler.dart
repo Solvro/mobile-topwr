b481aacba201bc8b96ea74c26ac8f13ecafe8c16
mikolaj-jalocha <mikics2k17@gmail.com>
2025-10-24 17:16:33 +0200

    feat: fix parking widget crashes on Android

--- lib/work_manager/background_task_handler.dart
+++ lib/work_manager/background_task_handler.dart
@@ -7,24 +7,14 @@ import "../features/home_screen_app_widgets/widget_service.dart";
 import "../features/parkings/parkings_view/api_client/iparking_client.dart";
 import "../features/parkings/parkings_view/repository/parkings_repository.dart";
 import "../utils/ilist_nonempty.dart";
-import "parking_widget_work_manager.dart";
 
 // TODO(mikolaj-jalocha): check if WorkManager can be replaced by AlarmManager
 @pragma("vm:entry-point")
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
     final Dio parkingClient = getIParkingClient();
-    if (ParkingWorkManagerTask.immediate.name == task) {
-      while (true) {
-        await _fetchDataAndUpdateParkingWidget(parkingClient);
-        await Future<void>.delayed(const Duration(seconds: 60));
-      }
-    } else if (ParkingWorkManagerTask.periodic.name == task) {
-      await _fetchDataAndUpdateParkingWidget(parkingClient);
-      return Future.value(true);
-    } else {
-      return Future.value(true);
-    }
+    await _fetchDataAndUpdateParkingWidget(parkingClient);
+    return Future.value(true);
   });
 }
 
--- lib/work_manager/parking_widget_work_manager.dart
+++ lib/work_manager/parking_widget_work_manager.dart
@@ -13,21 +13,9 @@ Future<void> setupParkingWidgetsWorkManager() async {
   }
   await Workmanager().initialize(callbackDispatcher);
 
-  // On Android minimum interval is 15 minutes (as of 10/08/2025).
-  // Thus we're launching here immediate worker that's enclosed in infinite loop with 60 sec interval
-  // 2nd one - periodic - is for safety. There's a chance that OS will kill immediate worker (it's not supposed to be enclosed in infinite loop), good to have alternative
-
-  if (Platform.isAndroid) {
-    await Workmanager().registerOneOffTask(
-      ParkingWorkManagerTask.immediate.name,
-      ParkingWorkManagerTask.immediate.name,
-      existingWorkPolicy: ExistingWorkPolicy.keep,
-    );
-  }
   await Workmanager().registerPeriodicTask(
     ParkingWorkManagerTask.periodic.name,
     ParkingWorkManagerTask.periodic.name,
     frequency: Duration(minutes: Platform.isAndroid ? 15 : 1),
-    existingWorkPolicy: ExistingWorkPolicy.keep,
   );
 }
