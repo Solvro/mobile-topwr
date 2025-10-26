84269b2c017181d0035b54749eb70359d61258cf
mikolaj-jalocha <mikics2k17@gmail.com>
2025-10-26 10:21:16 +0100

    feat: delete immediate worker

--- lib/work_manager/parking_widget_work_manager.dart
+++ lib/work_manager/parking_widget_work_manager.dart
@@ -5,7 +5,7 @@ import "package:workmanager/workmanager.dart";
 import "../features/home_screen_app_widgets/widget_service.dart";
 import "background_task_handler.dart";
 
-enum ParkingWorkManagerTask { immediate, periodic }
+enum ParkingWorkManagerTask { periodic }
 
 Future<void> setupParkingWidgetsWorkManager() async {
   if (Platform.isIOS) {
