d0f05da1c7df5123a9a64f517da50c35590e23aa
Anna Sidor <annasidor00@gmail.com>
2025-11-09 20:29:48 +0100

    temp: searching for a way to access path param in tab router view

--- lib/features/navigator/root_view.dart
+++ lib/features/navigator/root_view.dart
@@ -9,16 +9,41 @@ import "../../widgets/horizontal_symmetric_safe_area.dart";
 import "../app_changelog/update_changelog_wrapper.dart";
 import "../bottom_nav_bar/bottom_nav_bar.dart";
 import "nested_nav_pop_scope.dart";
+import "utils/route_params_controller.dart";
 
 @RoutePage()
 class RootView extends HookConsumerWidget {
-  const RootView({super.key, this.initialTabToGetBackTo = NavBarEnum.home, this.isFirstRootBottomView = true});
+  const RootView({
+    super.key,
+    this.initialTabToGetBackTo = NavBarEnum.home,
+    this.isFirstRootBottomView = true,
+    // @QueryParam("q") this.query,
+  });
+
+  // final String? query;
 
   final NavBarEnum initialTabToGetBackTo;
   final bool isFirstRootBottomView;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    print(context.router.current.name);
+    final query = Uri.parse(context.router.currentUrl).queryParameters["q"];
+    print("ROOT VIEW: $query");
+    if (query != null && context.router.current.name == "RootRoute") {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        // final current = context.router.current;
+        // final newUrl = Uri(
+        //   path: current.path,
+        //   queryParameters: {},
+        // ).toString();
+
+        // await context.router.replacePath(newUrl);
+        // print("replacing");
+        ref.read(routeParamsControllerProvider.notifier).onParamsChanged(query);
+      });
+    }
+
     final timesPushedToTabBar = useState(0);
     return ShowEntryDialogWrapper(
       child: AutoTabsRouter(
