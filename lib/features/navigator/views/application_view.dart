d10ad4451fd06444bd85a88557f8479516243b7d
24bartixx <jestgitbartix@gmail.com>
2025-11-09 19:42:14 +0100

    feat: nice global radio luz button widget

--- lib/features/navigator/views/application_view.dart
+++ lib/features/navigator/views/application_view.dart
@@ -2,6 +2,8 @@ import "package:auto_route/auto_route.dart";
 import "package:flutter/widgets.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
+import "../../radio_luz/presentation/radio_luz_global_button.dart";
+import "../../radio_luz/service/radio_player_controller.dart";
 import "../app_router.dart";
 import "../navigation_stack.dart";
 
@@ -12,12 +14,14 @@ class ApplicationView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final currentRoute = ref.watch(currentRouteProvider)?.settings.name;
+    final radioState = ref.watch(radioControllerProvider);
+    final showRadioLuzButton = radioState.isPlaying;
     final isRadioLuz = currentRoute == RadioLuzRoute.name;
 
     return Stack(
       children: [
         const AutoRouter(),
-        if (!isRadioLuz) const Positioned(right: 50, bottom: 100, child: Text("Hello")),
+        if (!isRadioLuz) const Positioned(right: 36, bottom: 96, child: RadioLuzGlobalButton()),
       ],
     );
   }
