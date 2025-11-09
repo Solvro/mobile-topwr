0cad239c6091d8b82bf0a2c63f08673355d862f4
24bartixx <jestgitbartix@gmail.com>
2025-11-09 19:19:25 +0100

    feat: global widget

--- lib/features/radio_luz/presentation/radio_luz_view.dart
+++ lib/features/radio_luz/presentation/radio_luz_view.dart
@@ -20,31 +20,33 @@ class RadioLuzView extends StatelessWidget {
     return Scaffold(
       backgroundColor: context.colorTheme.greyLight,
       appBar: RadioLuzAppBar(context, logoSize: 55),
-      body: Stack(
-        children: [
-          ListView(
-            padding: const EdgeInsets.all(16),
-            children: [
-              RadioLuzTitle(title: l10n.now_playing.toUpperCase()),
-              const SizedBox(height: 12),
-              const NowPlayingSection(),
-              const SizedBox(height: 24),
-              RadioLuzTitle(title: l10n.broadcast.toUpperCase()),
-              const SizedBox(height: 12),
-              const BroadcastsSection(),
-              const SizedBox(height: 20),
-              RadioLuzTitle(title: l10n.radio_luz_info.toUpperCase()),
-              const SizedBox(height: 12),
-              Text(l10n.radio_luz_description1, style: context.textTheme.body),
-              const SizedBox(height: 2),
-              Text(l10n.radio_luz_description2, style: context.textTheme.body),
-              const SizedBox(height: 12),
-              const RadioLuzSocialsSection(),
-              const SizedBox(height: 80),
-            ],
-          ),
-          const Align(alignment: Alignment.bottomCenter, child: AudioPlayerWidget()),
-        ],
+      body: SafeArea(
+        child: Stack(
+          children: [
+            ListView(
+              padding: const EdgeInsets.all(16),
+              children: [
+                RadioLuzTitle(title: l10n.now_playing.toUpperCase()),
+                const SizedBox(height: 12),
+                const NowPlayingSection(),
+                const SizedBox(height: 24),
+                RadioLuzTitle(title: l10n.broadcast.toUpperCase()),
+                const SizedBox(height: 12),
+                const BroadcastsSection(),
+                const SizedBox(height: 20),
+                RadioLuzTitle(title: l10n.radio_luz_info.toUpperCase()),
+                const SizedBox(height: 12),
+                Text(l10n.radio_luz_description1, style: context.textTheme.body),
+                const SizedBox(height: 2),
+                Text(l10n.radio_luz_description2, style: context.textTheme.body),
+                const SizedBox(height: 12),
+                const RadioLuzSocialsSection(),
+                const SizedBox(height: 80),
+              ],
+            ),
+            const Align(alignment: Alignment.bottomCenter, child: AudioPlayerWidget()),
+          ],
+        ),
       ),
     );
   }
