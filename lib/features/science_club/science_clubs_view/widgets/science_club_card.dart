0f46c965bbb38256ac814cece508ab4b8134a853
Anna Sidor <annasidor00@gmail.com>
2025-11-10 19:48:12 +0100

    feat: better behaving student club tile

--- lib/features/science_club/science_clubs_view/widgets/science_club_card.dart
+++ lib/features/science_club/science_clubs_view/widgets/science_club_card.dart
@@ -3,10 +3,12 @@ import "package:flutter/material.dart";
 
 import "../../../../config/ui_config.dart";
 import "../../../../theme/app_theme.dart";
+import "../../../../utils/calculate_lines.dart";
 import "../../../../utils/context_extensions.dart";
 import "../../../../widgets/my_cached_image.dart";
 import "../../../../widgets/rest_api_image.dart";
 import "../model/science_clubs.dart";
+import "../utils/scale_extension.dart";
 import "strategic_badge.dart";
 import "verified_badge.dart";
 
@@ -19,21 +21,11 @@ class ScienceClubCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final title = sciClub.name;
-    final subtitle = sciClub.department?.name;
-    final secondSubtitle = sciClub.tags?.map((tag) => "#${tag.tag}").toList().join(", ");
+    final department = sciClub.department?.name;
+    final tags = sciClub.tags?.map((tag) => "#${tag.tag}").toList().join(", ");
     final showVerifiedBadge = sciClub.source == ScienceClubSource.manualEntry;
     final showStrategicBadge = sciClub.isStrategic;
 
-    final placeholderDimensions = [
-      if (showVerifiedBadge) const PlaceholderDimensions(size: Size(12, 12), alignment: PlaceholderAlignment.middle),
-      if (showStrategicBadge) const PlaceholderDimensions(size: Size(12, 12), alignment: PlaceholderAlignment.middle),
-    ];
-
-    TextSpan titleSpan({double? fontSize}) => TextSpan(
-      text: title,
-      style: context.textTheme.title,
-      children: [if (showVerifiedBadge) const VerifiedBadge(), if (showStrategicBadge) const StrategicBadge()],
-    );
     return ClipRRect(
       borderRadius: const BorderRadius.all(WideTileCardConfig.radius),
       child: Material(
@@ -44,74 +36,135 @@ class ScienceClubCard extends StatelessWidget {
               color: context.colorTheme.greyLight,
               borderRadius: const BorderRadius.all(WideTileCardConfig.radius),
             ),
-            child: SizedBox(
-              height: 440,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: WideTileCardConfig.basePadding,
-                        top: WideTileCardConfig.basePadding,
-                        right: WideTileCardConfig.basePadding * 1.04,
-                        bottom: WideTileCardConfig.basePadding,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          AutoSizeText.rich(
-                            titleSpan(),
-                            placeholderDimensions: placeholderDimensions,
-                            maxLines: 3,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          if (subtitle != null) const SizedBox(height: 2),
-                          if (subtitle != null)
-                            AutoSizeText.rich(
-                              TextSpan(text: subtitle, style: context.textTheme.body),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          if (secondSubtitle != null) const SizedBox(height: 2),
-                          if (secondSubtitle != null)
-                            AutoSizeText.rich(
-                              TextSpan(text: secondSubtitle, style: context.textTheme.bodyBlue),
-                              maxLines: 2,
-                              maxFontSize: 12,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                        ],
-                      ),
-                    ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: _TextContent(
+                    title: title,
+                    department: department,
+                    tags: tags,
+                    showVerifiedBadge: showVerifiedBadge,
+                    showStrategicBadge: showStrategicBadge,
                   ),
-                  const SizedBox(width: 16),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      right: ScienceClubCardConfig.trailingPadding,
-                      top: ScienceClubCardConfig.trailingPadding,
-                      bottom: ScienceClubCardConfig.trailingPadding,
-                    ),
-                    child: SizedBox(
-                      width: WideTileCardConfig.imageSize,
-                      height: context.textScaler.scale(WideTileCardConfig.imageSize),
-                      child: Container(
-                        clipBehavior: Clip.antiAlias,
-                        decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topRight: WideTileCardConfig.radius,
-                            bottomRight: WideTileCardConfig.radius,
-                          ),
+                ),
+                const SizedBox(width: ScienceClubCardConfig.contentImageSpacing),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: ScienceClubCardConfig.trailingPadding,
+                    top: ScienceClubCardConfig.trailingPadding,
+                    bottom: ScienceClubCardConfig.trailingPadding,
+                  ),
+                  child: SizedBox(
+                    width: ScienceClubCardConfig.imageWidth,
+                    height: context.textScaler.scale(ScienceClubCardConfig.imageHeight),
+                    child: Container(
+                      clipBehavior: Clip.antiAlias,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: WideTileCardConfig.radius,
+                          bottomRight: WideTileCardConfig.radius,
                         ),
-                        child: RestApiImage(sciClub.logo, boxFit: BoxFit.scaleDown, loadingType: LoadingType.noLoading),
                       ),
+                      child: RestApiImage(sciClub.logo, boxFit: BoxFit.scaleDown, loadingType: LoadingType.noLoading),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _TextContent extends StatelessWidget {
+  final String title;
+  final String? department;
+  final String? tags;
+  final bool showVerifiedBadge;
+  final bool showStrategicBadge;
+
+  const _TextContent({
+    required this.title,
+    this.department,
+    this.tags,
+    required this.showVerifiedBadge,
+    required this.showStrategicBadge,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // if you look into the badges, they have sizes of 12 or 14 and a 4 left padding, hence the math
+    final placeholderDimensions = [
+      if (showVerifiedBadge) const PlaceholderDimensions(size: Size(16, 12), alignment: PlaceholderAlignment.middle),
+      if (showStrategicBadge) const PlaceholderDimensions(size: Size(18, 12), alignment: PlaceholderAlignment.middle),
+    ];
+
+    TextSpan titleSpan({double? fontSize}) => TextSpan(
+      text: title,
+      style: fontSize != null ? context.textTheme.title.copyWith(fontSize: fontSize) : context.textTheme.title,
+      children: [if (showVerifiedBadge) const VerifiedBadge(), if (showStrategicBadge) const StrategicBadge()],
+    );
+
+    return Padding(
+      padding: const EdgeInsets.all(WideTileCardConfig.basePadding),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final titleLines = TextSpan(text: title, style: context.textTheme.title).calculateLines(constraints.maxWidth);
+          final badgesIn2ndLine =
+              placeholderDimensions.isNotEmpty &&
+              titleSpan(fontSize: 18).calculateLines(
+                    constraints.maxWidth -
+                        (showStrategicBadge ? 18 : 0) -
+                        (showVerifiedBadge
+                            ? 16
+                            : 0), // if you look into the badges, they have sizes of 12 or 14 and a 4 left padding, hence the math
+                    placeholderDimensions: placeholderDimensions,
+                  ) >
+                  titleLines;
+          final int actualTitleLines = badgesIn2ndLine ? titleLines + 1 : titleLines;
+
+          final scaleType = context.textScaleFactor.scaleType;
+          final maxLines = calculateMaxLines(
+            scaleType: scaleType,
+            hasTags: tags != null,
+            hasDepts: department != null,
+            titleLines: actualTitleLines,
+          );
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              AutoSizeText.rich(
+                titleSpan(),
+                placeholderDimensions: placeholderDimensions,
+                maxLines: maxLines.titleMaxLines,
+                maxFontSize: 18,
+                overflow: TextOverflow.ellipsis,
+              ),
+              if (department != null && maxLines.deptsMaxLines > 0)
+                const SizedBox(height: ScienceClubCardConfig.trailingPadding),
+              if (department != null && maxLines.deptsMaxLines > 0)
+                AutoSizeText.rich(
+                  TextSpan(text: department, style: context.textTheme.body),
+                  maxLines: maxLines.deptsMaxLines,
+                  maxFontSize: 13,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              if (tags != null && maxLines.tagsMaxLines > 0)
+                const SizedBox(height: ScienceClubCardConfig.trailingPadding),
+              if (tags != null && maxLines.tagsMaxLines > 0)
+                AutoSizeText.rich(
+                  TextSpan(text: tags, style: context.textTheme.bodyBlue),
+                  maxLines: maxLines.tagsMaxLines,
+                  maxFontSize: 12,
+                  overflow: TextOverflow.ellipsis,
+                ),
+            ],
+          );
+        },
       ),
     );
   }
