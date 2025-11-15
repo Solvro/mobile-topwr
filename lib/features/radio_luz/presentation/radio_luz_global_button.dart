e9ec3edb796a21988e2d5df5f5d26f86b1bbdf73
24bartixx <jestgitbartix@gmail.com>
2025-11-15 14:01:30 +0100

    fix: fab icon not logo

--- lib/features/radio_luz/presentation/radio_luz_global_button.dart
+++ lib/features/radio_luz/presentation/radio_luz_global_button.dart
@@ -1,9 +1,10 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:flutter_svg/svg.dart";
 
+import "../../../gen/assets.gen.dart";
 import "../../../theme/app_theme.dart";
 import "../../navigator/utils/navigation_commands.dart";
-import "radio_luz_logo.dart";
 
 class RadioLuzGlobalButton extends ConsumerWidget {
   const RadioLuzGlobalButton({super.key});
@@ -12,14 +13,19 @@ class RadioLuzGlobalButton extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialButton(
       elevation: 1,
-      padding: const EdgeInsets.all(8),
+      padding: const EdgeInsets.symmetric(vertical: 8),
       onPressed: () async {
         await ref.navigateToRadioLuz();
       },
       color: context.colorTheme.orangePomegranade,
       textColor: context.colorTheme.whiteSoap,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-      child: RadioLuzLogo(logoColor: context.colorTheme.whiteSoap, logoSize: 24),
+      child: SvgPicture.asset(
+        Assets.svg.radioLuz.fabIcon,
+        colorFilter: ColorFilter.mode(context.colorTheme.whiteSoap, BlendMode.srcIn),
+        height: 24,
+        width: 24,
+      ),
     );
   }
 }
