60eb8c2383b082dccc698a4e958ad7dfde673072
MarmoPL <marcin.czembr@gmail.com>
2025-12-10 02:08:33 +0100

    fix: link

--- lib/features/radio_luz/presentation/radio_luz_socials_section.dart
+++ lib/features/radio_luz/presentation/radio_luz_socials_section.dart
@@ -5,38 +5,15 @@ import "package:hooks_riverpod/hooks_riverpod.dart";
 import "../../../gen/assets.gen.dart";
 import "../../../theme/app_theme.dart";
 import "../../../utils/launch_url_util.dart";
-import "../../branches/data/model/branch.dart";
-import "../../navigator/utils/navigation_commands.dart";
-import "../../science_club/science_clubs_filters/model/sci_club_type.dart";
-import "../../science_club/science_clubs_view/model/science_clubs.dart";
 
-typedef SocialIconData = ({String assetPath, String url, bool inApp, ScienceClub? scienceClub});
+typedef SocialIconData = ({String assetPath, String url});
 
 final List<SocialIconData> _socialIconsData = [
-  (assetPath: Assets.svg.contactIcons.fb, url: "https://www.facebook.com/radioluz", inApp: false, scienceClub: null),
-  (
-    assetPath: Assets.svg.contactIcons.yt,
-    url: "https://www.youtube.com/@AkademickieRadioLUZ",
-    inApp: false,
-    scienceClub: null,
-  ),
-  (
-    assetPath: Assets.svg.logoAppBar,
-    url: "",
-    inApp: true,
-    scienceClub: const ScienceClub(
-      id: 1365,
-      name: "",
-      organizationStatus: ScienceClubStatus.active,
-      source: ScienceClubSource.manualEntry,
-      organizationType: ScienceClubType.studentOrganization,
-      coverPreview: false,
-      isStrategic: false,
-      branch: Branch.main,
-    ),
-  ),
-  (assetPath: Assets.svg.contactIcons.ig, url: "https://www.instagram.com/radio_luz/", inApp: false, scienceClub: null),
-  (assetPath: Assets.svg.contactIcons.web, url: "https://radioluz.pl/", inApp: false, scienceClub: null),
+  (assetPath: Assets.svg.contactIcons.fb, url: "https://www.facebook.com/radioluz"),
+  (assetPath: Assets.svg.contactIcons.yt, url: "https://www.youtube.com/@AkademickieRadioLUZ"),
+  (assetPath: Assets.svg.logoAppBar, url: "https://topwr.solvro.pl/sci-clubs/1365"),
+  (assetPath: Assets.svg.contactIcons.ig, url: "https://www.instagram.com/radio_luz/"),
+  (assetPath: Assets.svg.contactIcons.web, url: "https://radioluz.pl/"),
 ];
 
 class RadioLuzSocialsSection extends ConsumerWidget {
@@ -53,9 +30,7 @@ class RadioLuzSocialsSection extends ConsumerWidget {
         (index) => Padding(
           padding: EdgeInsets.only(right: index == _socialIconsData.length - 1 ? 0 : 16),
           child: InkWell(
-            onTap: () => _socialIconsData[index].inApp
-                ? ref.navigateSciClubsDetail(_socialIconsData[index].scienceClub!)
-                : ref.launch(_socialIconsData[index].url),
+            onTap: () => ref.launch(_socialIconsData[index].url),
             child: SvgPicture.asset(
               _socialIconsData[index].assetPath,
               width: iconSize,
