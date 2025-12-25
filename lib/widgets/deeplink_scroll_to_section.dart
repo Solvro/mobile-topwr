71fbf876d283be4e8af5a6f1c9f7c17211c1a53c
Szymon Kowaliński <szymon@kowalinski.dev>
2025-12-25 21:16:48 +0100

    refactor: use proper flutter hooks

--- lib/widgets/deeplink_scroll_to_section.dart
+++ lib/widgets/deeplink_scroll_to_section.dart
@@ -1,13 +1,13 @@
 import "dart:async";
 
 import "package:flutter/material.dart";
+import "package:flutter_hooks/flutter_hooks.dart";
 
 class DeeplinkScrollToSection {
-  DeeplinkScrollToSection({this.sectionId});
+  DeeplinkScrollToSection({required this.scrollController, this.sectionId});
 
   final int? sectionId;
-
-  final scrollController = ScrollController();
+  final ScrollController scrollController;
   final Map<int, GlobalKey> _sectionKeys = {};
 
   void registerSections(Iterable<int> ids) {
@@ -27,39 +27,24 @@ class DeeplinkScrollToSection {
   }
 
   bool get shouldScroll => sectionId != null && sectionId! > 0 && _sectionKeys.containsKey(sectionId);
-
-  void dispose() => scrollController.dispose();
 }
 
 // Wrapper widget to allow using DeeplinkScrollToSection in stateless widgets(less refactors)
-class DeeplinkScrollToSectionWrapper extends StatefulWidget {
+class DeeplinkScrollToSectionWrapper extends HookWidget {
   const DeeplinkScrollToSectionWrapper({super.key, required this.builder, this.sectionId});
 
   final int? sectionId;
   final Widget Function(BuildContext, DeeplinkScrollToSection) builder;
 
   @override
-  State<DeeplinkScrollToSectionWrapper> createState() => _DeeplinkScrollToSectionWrapperState();
-}
-
-class _DeeplinkScrollToSectionWrapperState extends State<DeeplinkScrollToSectionWrapper> {
-  late final DeeplinkScrollToSection helper;
-
-  @override
-  void initState() {
-    super.initState();
-    helper = DeeplinkScrollToSection(sectionId: widget.sectionId);
-  }
-
-  @override
-  void dispose() {
-    helper.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final w = widget.builder(context, helper);
+    final scrollController = useScrollController();
+    final helper = useMemoized(
+      () => DeeplinkScrollToSection(scrollController: scrollController, sectionId: sectionId),
+      [sectionId],
+    );
+
+    final w = builder(context, helper);
     if (helper.shouldScroll) {
       WidgetsBinding.instance.addPostFrameCallback((_) => helper.scrollToSection());
     }
