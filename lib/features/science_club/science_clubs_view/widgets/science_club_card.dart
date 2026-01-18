7fe5694bcaa6b9d33e849c59aa1ec8afcf37d33f
Szymon Kowaliński <szymon@kowalinski.dev>
2026-01-18 13:27:00 +0100

    fix(sci-clubs): new tile scaling logic (#995)

--- lib/features/science_club/science_clubs_view/widgets/science_club_card.dart
+++ lib/features/science_club/science_clubs_view/widgets/science_club_card.dart
@@ -1,11 +1,12 @@
 import "package:flutter/material.dart";
 
 import "../../../../config/ui_config.dart";
+import "../../../../theme/app_theme.dart";
 import "../../../../utils/context_extensions.dart";
 import "../../../../widgets/my_cached_image.dart";
 import "../../../../widgets/rest_api_image.dart";
-import "../../../../widgets/wide_tile_card.dart";
 import "../model/science_clubs.dart";
+import "science_club_text.dart";
 
 class ScienceClubCard extends StatelessWidget {
   final ScienceClub sciClub;
@@ -15,32 +16,72 @@ class ScienceClubCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WideTileCard(
-      title: sciClub.name,
-      subtitle: sciClub.department?.name,
-      onTap: onTap,
-      secondSubtitle: sciClub.tags?.map((tag) => "#${tag.tag}").toList().join(", "),
-      showBadge: sciClub.source == ScienceClubSource.manualEntry,
-      showStrategicBadge: sciClub.isStrategic,
-      activeShadows: null,
-      trailing: Padding(
-        padding: const EdgeInsets.only(
-          right: ScienceClubCardConfig.trailingPadding,
-          top: ScienceClubCardConfig.trailingPadding,
-          bottom: ScienceClubCardConfig.trailingPadding,
-        ),
-        child: SizedBox(
-          width: WideTileCardConfig.imageSize,
-          height: context.textScaler.scale(WideTileCardConfig.imageSize),
-          child: Container(
-            clipBehavior: Clip.antiAlias,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topRight: WideTileCardConfig.radius,
-                bottomRight: WideTileCardConfig.radius,
+    final subtitle = [
+      ...?sciClub.tags?.map((tag) => "#${tag.tag}"),
+      if (sciClub.department?.code != null) "#${sciClub.department?.code}",
+    ].join(", ");
+
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(WideTileCardConfig.radius),
+      child: Material(
+        child: InkWell(
+          onTap: onTap,
+          child: Ink(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surfaceTint,
+              borderRadius: const BorderRadius.all(WideTileCardConfig.radius),
+            ),
+            child: SizedBox(
+              height: context.textScaler.clamp(maxScaleFactor: 2).scale(WideTileCardConfig.imageSize),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: WideTileCardConfig.basePadding,
+                        top: WideTileCardConfig.basePadding,
+                        right: WideTileCardConfig.basePadding * 1.04,
+                      ),
+                      child: ScienceClubText(
+                        title: sciClub.name,
+                        titleStyle: Theme.of(context).textTheme.titleLarge,
+                        subtitle: subtitle,
+                        subtitleStyle: Theme.of(
+                          context,
+                        ).textTheme.bodyLarge?.copyWith(color: context.colorScheme.secondary),
+                        spacing: WideTileCardConfig.titlesSpacing,
+                        maxTotalLines: context.textScaleFactor > 1.5 ? 5 : 4,
+                        showVerifiedBadge: sciClub.source == ScienceClubSource.manualEntry,
+                        showStrategicBadge: sciClub.isStrategic,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      right: ScienceClubCardConfig.trailingPadding,
+                      top: ScienceClubCardConfig.trailingPadding,
+                      bottom: ScienceClubCardConfig.trailingPadding,
+                    ),
+                    child: SizedBox(
+                      width: WideTileCardConfig.imageSize,
+                      height: context.textScaler.scale(WideTileCardConfig.imageSize),
+                      child: Container(
+                        clipBehavior: Clip.antiAlias,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topRight: WideTileCardConfig.radius,
+                            bottomRight: WideTileCardConfig.radius,
+                          ),
+                        ),
+                        child: RestApiImage(sciClub.logo, boxFit: BoxFit.scaleDown, loadingType: LoadingType.noLoading),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            child: RestApiImage(sciClub.logo, boxFit: BoxFit.scaleDown, loadingType: LoadingType.noLoading),
           ),
         ),
       ),
