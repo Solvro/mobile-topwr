2e0f7bab07b6279d9480ec1d67099df52e4f1665
Szymon Kowaliński <szymon@kowalinski.dev>
2026-02-08 17:18:24 +0100

    refactor(sks-favs): proper provider for device id retrieval (#1022)

--- lib/features/sks/sks_favourite_dishes/utils/sks_favourite_dishes_extension.dart
+++ lib/features/sks/sks_favourite_dishes/utils/sks_favourite_dishes_extension.dart
@@ -1,12 +1,15 @@
 import "package:dio/dio.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 
+import "../../../../api_base_rest/client/dio_client.dart";
 import "../../../../config/env.dart";
 import "../../../../utils/get_device_id.dart";
 
-extension DioFetchSksFavouriteDishes on Dio {
+extension DioFetchSksFavouriteDishes on Ref {
   Future<void> toggleSubscription(String dishId, {required bool isSubscribed}) async {
+    final dio = read(restClientProvider);
     final url = "${Env.sksUrl}/subscriptions/toggle";
-    final key = await getDeviceId();
+    final key = await watch(getDeviceIdProvider.future);
     if (key == null) {
       throw DioException(
         requestOptions: RequestOptions(path: url),
@@ -14,7 +17,7 @@ extension DioFetchSksFavouriteDishes on Dio {
       );
     }
     final dishIdNum = int.tryParse(dishId);
-    await post<Map<String, dynamic>>(
+    await dio.post<Map<String, dynamic>>(
       url,
       data: {"deviceKey": key, "mealId": dishIdNum ?? dishId, "subscribe": isSubscribed},
     );
