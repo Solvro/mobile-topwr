a762eef33319a50b8564743fed43fbe1f2904c88
Bartosz Żurawski <92211556+BombardierBulge@users.noreply.github.com>
2026-03-01 21:23:08 +0100

    fix: implement focus management for accessibility in notification modal (#1040)
    * fix: implement focus management for accessibility
    
    * fix: remove redundant type declaration for focus node to pass CI check
    
    * fix: refactor NotificationButton to use hooks for focus management

--- lib/widgets/my_splash_tile.dart
+++ lib/widgets/my_splash_tile.dart
@@ -4,16 +4,19 @@ import "../config/ui_config.dart";
 import "../theme/app_theme.dart";
 
 class MySplashTile extends StatelessWidget {
-  const MySplashTile({super.key, required this.child, this.onTap, this.backgroundColor});
+  const MySplashTile({super.key, required this.child, this.onTap, this.backgroundColor, this.focusNode});
 
   final Widget child;
   final VoidCallback? onTap;
   final Color? backgroundColor;
+  final FocusNode? focusNode;
+
   @override
   Widget build(BuildContext context) {
     return Material(
       borderRadius: const BorderRadius.all(WideTileCardConfig.radius),
       child: InkWell(
+        focusNode: focusNode,
         borderRadius: const BorderRadius.all(WideTileCardConfig.radius),
         onTap: onTap,
         child: Ink(
