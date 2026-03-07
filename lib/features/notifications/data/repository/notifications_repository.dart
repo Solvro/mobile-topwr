94c1f73cc48ad736568824d165dd44c2eec6fe6c
Szymon Kowaliński <szymon@kowalinski.dev>
2026-03-07 21:10:37 +0100

    feat(notifications): add notifications list view (#1079)

--- lib/features/notifications/data/repository/notifications_repository.dart
+++ lib/features/notifications/data/repository/notifications_repository.dart
@@ -1,4 +1,3 @@
-import "package:collection/collection.dart";
 import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
@@ -11,27 +10,16 @@ import "../models/notification_model.dart";
 part "notifications_repository.g.dart";
 
 @riverpod
-class NotificationsRepository extends _$NotificationsRepository {
-  Future<void> clearCache() async {
-    await ref.clearCache("${Env.mainRestApiUrl}/notification", TtlDays.notifications);
-  }
-
-  @override
-  Future<IList<NotificationModel>> build() async {
-    final url = "${Env.mainRestApiUrl}/notification";
-    final allNotifications = await ref
-        .getAndCacheData(
-          url,
-          NotificationModel.fromJson,
-          ttlDays: TtlDays.notifications,
-          extraValidityCheck: (_) => true,
-          onRetry: ref.invalidateSelf,
-        )
-        .castAsList;
-
-    final sortedNotifications = allNotifications
-        .where((notification) => notification.wasSent)
-        .sortedByCompare((notification) => notification.createdAt, (a, b) => b.compareTo(a));
-    return sortedNotifications.toIList();
-  }
+Future<IList<NotificationModel>> notificationsRepository(Ref ref) async {
+  final url = "${Env.mainRestApiUrl}/notification";
+  final allNotifications = await ref
+      .getAndCacheData(
+        url,
+        NotificationModel.fromJson,
+        ttlDays: TtlDays.notifications,
+        extraValidityCheck: (_) => true,
+        onRetry: ref.invalidateSelf,
+      )
+      .castAsList;
+  return allNotifications.where((notification) => notification.wasSent).toIList();
 }
