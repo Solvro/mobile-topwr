3f038faa2bf7ff6c1c9bcf9973961d742a9c8185
MarmoPL <marcin.czembr@gmail.com>
2026-03-15 22:17:36 +0100

    fix: tests

--- lib/features/radio_luz/data/repository/history_entry_repository.dart
+++ lib/features/radio_luz/data/repository/history_entry_repository.dart
@@ -1,4 +1,5 @@
 import "package:fast_immutable_collections/fast_immutable_collections.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart" show Provider;
 import "package:riverpod_annotation/riverpod_annotation.dart";
 
 import "../../../../utils/ref_extensions.dart";
@@ -8,12 +9,16 @@ import "radio_luz_repository.dart";
 
 part "history_entry_repository.g.dart";
 
+final radioTrackChangedProvider = Provider<Stream<String>>((ref) {
+  return ref.read(radioPlayerProvider).trackChanged;
+});
+
 @riverpod
 Future<IList<HistoryEntry>?> historyEntryRepository(Ref ref) async {
   ref.setRefresh(const Duration(seconds: 60));
 
-  final handler = ref.read(radioPlayerProvider);
-  final sub = handler.trackChanged.listen((_) => ref.invalidateSelf());
+  final trackStream = ref.read(radioTrackChangedProvider);
+  final sub = trackStream.listen((_) => ref.invalidateSelf());
   ref.onDispose(sub.cancel);
 
   final repository = ref.watch(radioLuzRepositoryProvider);
