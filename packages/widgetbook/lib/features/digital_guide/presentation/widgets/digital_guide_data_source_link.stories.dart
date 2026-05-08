89caa00ac79e8d3a05277e43ac5e268706dae970
Szymon Kowaliński <szymon@kowalinski.dev>
2026-05-08 23:51:11 +0200

    feat: more stories

---
+++ packages/widgetbook/lib/features/digital_guide/presentation/widgets/digital_guide_data_source_link.stories.dart
@@ -0,0 +1,11 @@
+import "package:flutter/widgets.dart";
+import "package:topwr/features/digital_guide/presentation/widgets/digital_guide_data_source_link.dart";
+import "package:widgetbook/widgetbook.dart";
+
+part "digital_guide_data_source_link.stories.g.dart";
+
+const meta = Meta<DigitalGuideDataSourceLink>();
+
+final $default = DigitalGuideDataSourceLinkStory(
+  setup: (context, child, args) => Center(child: child),
+);
---
+++ packages/widgetbook/lib/features/map_view/widgets/map_tile_layer.stories.dart
@@ -0,0 +1,20 @@
+import "package:flutter/widgets.dart";
+import "package:flutter_map/flutter_map.dart";
+import "package:latlong2/latlong.dart";
+import "package:topwr/features/map_view/widgets/map_widget.dart";
+import "package:widgetbook/widgetbook.dart";
+
+part "map_tile_layer.stories.g.dart";
+
+const meta = Meta<MapTileLayer>();
+
+final $default = MapTileLayerStory(
+  setup: (context, child, args) => SizedBox(
+    width: 390,
+    height: 390,
+    child: FlutterMap(
+      options: const MapOptions(initialCenter: LatLng(51.1079, 17.0614), initialZoom: 15),
+      children: [child],
+    ),
+  ),
+);
