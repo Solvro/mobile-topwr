b39de42f969f93b8710fd4175ce52f7dee9f1678
Maksymilian Mazurkiewicz <maksymilian.mazurkiewicz@vality.pl>
2026-06-07 21:02:25 +0200

    fix: sks favs headers text overflow

---
+++ packages/widgetbook/lib/features/sks/sks_favourite_dishes/presentation/widgets/sliver_sticky_header.stories.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
+import "package:topwr/features/sks/sks_favourite_dishes/presentation/widgets/sliver_sticky_header.dart";
+import "package:widgetbook/widgetbook.dart";
+
+part "sliver_sticky_header.stories.g.dart";
+
+const meta = Meta<SliverStickyHeader>();
+
+final $default = SliverStickyHeaderStory(
+  args: SliverStickyHeaderArgs.fixed(child: const Text("Lorem ipsum")),
+  setup: (context, child, args) {
+    return CustomScrollView(slivers: [child]);
+  },
+);
