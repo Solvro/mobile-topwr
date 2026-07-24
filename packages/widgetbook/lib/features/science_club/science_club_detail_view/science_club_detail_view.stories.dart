134d23938dfcbad4d83c713c621675ddb756d521
Mikołaj Rudnicki <mikolaj@rudnicki.dev>
2026-07-24 22:08:34 +0200

    fix(sci-clubs): add widgetbook missing imports

--- packages/widgetbook/lib/features/science_club/science_club_detail_view/science_club_detail_view.stories.dart
+++ packages/widgetbook/lib/features/science_club/science_club_detail_view/science_club_detail_view.stories.dart
@@ -1,5 +1,7 @@
+import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:flutter/widgets.dart";
 import "package:topwr/features/science_club/science_club_detail_view/science_club_detail_view.dart";
+import "package:topwr/features/science_club/science_clubs_view/model/science_clubs.dart";
 import "package:widgetbook/widgetbook.dart";
 
 part "science_club_detail_view.stories.g.dart";
